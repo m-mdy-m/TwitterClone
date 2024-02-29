5ad0becd34f56076f0ae9a91907d20559592e09e
mahdi <mahdimamashli1383@gmail.com>
2024-02-29 18:26:09 +0330

    Replace `require` with `$read` for `is-auth` middleware.

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,10 @@
 const Xprz = require("xprz");
-const isAuth = require("../middleware/is-auth").isAuth;
+const isAuth = $read("middleware/is-auth").isAuth;
 const { Route } = new Xprz();
 const route = new Route();
 route
   .setRoute("/")
-  .using(isAuth)
+  .using([isAuth])
   .get(() => {
     const { status } = route.res();
     status(200).render("home", {
