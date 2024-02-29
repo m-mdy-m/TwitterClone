98358309e352cb80825746f5ae453daae56c6c17
mahdi <mahdimamashli1383@gmail.com>
2024-02-29 17:47:36 +0330

    Update to load routes with strict mode enabled.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Xprz = require("xprz");
-const { App, HttpMethod, Route } = new Xprz();
+const { App } = new Xprz();
 const { launch, loadRoutes, setTemplateEngine } = new App();
 launch();
 setTemplateEngine().ejs();
-loadRoutes("routes");
+loadRoutes("routes", true);
