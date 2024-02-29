31d77410f76d09a31bd027acad196bf201729b21
mahdi <mahdimamashli1383@gmail.com>
2024-02-29 19:02:03 +0330

    Changed the `ensureAuthenticated` function to an exported arrow function. Removed the `return` keyword before `nxt()` when the user is authenticated. Added an `else` block to redirect unauthenticated users.

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,9 +1,9 @@
-function ensureAuthenticated(statuscode = 401, redirect = "/signup") {
+exports.ensureAuthenticated = (statuscode = 401, redirect = "/signup") => {
   return (req, res, nxt) => {
     if (req.session && req.session.user) {
-      return nxt();
+      nxt();
+    } else {
+      res.status(statuscode).redirect(redirect);
     }
-    return res.status(statuscode).redirect(redirect);
   };
-}
-module.exports = {ensureAuthenticated }
+};
