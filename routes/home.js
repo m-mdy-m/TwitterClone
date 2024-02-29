6b880ccc1b7d76466d4faab8594b1d6fb57e6a9e
mahdi <mahdimamashli1383@gmail.com>
2024-02-29 18:12:26 +0330

    Add authentication middleware to the home route.

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,15 @@
 const Xprz = require("xprz");
-
+const isAuth = require("../middleware/is-auth").isAuth;
 const { Route } = new Xprz();
 const route = new Route();
-route.setRoute("/").get(() => {
-  const { status } = route.res();
-  status(200).render("home",{
-    Title : "Home"
+route
+  .setRoute("/")
+  .using(isAuth)
+  .get(() => {
+    const { status } = route.res();
+    status(200).render("home", {
+      Title: "Home",
+    });
   });
-});
 
 module.exports = route;
