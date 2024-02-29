9f8dd34ae64a51923645c814db5727eded443d21
mahdi <mahdimamashli1383@gmail.com>
2024-02-29 19:37:57 +0330

    Added static middleware to serve public files.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const Xprz = require("xprz");
 const { App } = new Xprz();
-const { launch, loadRoutes, setTemplateEngine, useJsonBody } = new App();
+const { launch, loadRoutes, setTemplateEngine, useJsonBody,static } = new App();
 launch(); // start server
 useJsonBody();
 setTemplateEngine().ejs(); // set template
+static('public')
 loadRoutes("routes"); // load all file routes
---
+++ routes/login.js
@@ -0,0 +1,4 @@
+const Xprz = require("xprz");
+const { Route } = new Xprz();
+const route = new Route();
+module.exports = route
