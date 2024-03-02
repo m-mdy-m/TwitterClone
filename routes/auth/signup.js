4100867e5fb5a6ee387de66641c0c2a7aaab6cc1
mahdi <mahdimamashli1383@gmail.com>
2024-03-02 18:44:53 +0330

    Added a POST route to the signup page to handle user input.

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,10 +1,17 @@
 const Xprz = require("xprz");
 const { Route } = new Xprz();
 const route = new Route();
-route.setRoute("/signup").get(() => {
-  const { status } = route.res();
-  status(200).render("auth/signup.ejs", {
-    Title: "signup",
+route
+  .setRoute("/signup")
+  .get(() => {
+    const { status } = route.res();
+    status(200).render("auth/signup.ejs", {
+      Title: "signup",
+    });
+  })
+  .post(() => {
+    const { getBody } = route.req();
+    const body = getBody();
+    console.log("body=>", body);
   });
-});
 module.exports = route;
