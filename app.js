7959c7cdd2bc18f5c860016b6125cd4d4f333a5e
mahdi <mahdimamashli1383@gmail.com>
2024-03-02 19:00:21 +0330

    Refactored code to use destructuring for commonly used methods from the App instance.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// Importing the Xprz framework module
 const Xprz = require("xprz");
+// Creating a new instance of the Xprz framework
 const { App } = new Xprz();
-const { launch, loadRoutes, setTemplateEngine, useJsonBody,static } = new App();
-launch(); // start server
-useJsonBody();
-setTemplateEngine().ejs(); // set template
-static('public') // add public static file
-loadRoutes("routes"); // load all file routes
+// Destructuring commonly used methods from the App instance
+const { launch, loadRoutes, setTemplateEngine, useJsonBody, static } = new App();
+launch(); // Launching the server
+useJsonBody(); // Parsing JSON request bodies
+setTemplateEngine().ejs(); // Setting up the template engine (using EJS in this case)
+static('public') // Serving static files from the 'public' directory
+loadRoutes("routes"); // Loading routes from the 'routes' directory
