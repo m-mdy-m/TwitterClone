e70409eaaa6132c67435b715ff6b0871ce87faf6
mahdi <mahdimamashli1383@gmail.com>
2024-03-02 17:23:05 +0330

    Add validation count to the form.

--- public/js/auth/auth.js
+++ public/js/auth/auth.js
@@ -1,9 +1,17 @@
 const formGroup = document.querySelectorAll(".form-group");
+const form = document.getElementById("registerForm");
 const msgErrors = document.querySelectorAll(".msg-error");
 const valid = "#90EE90";
 const invalid = "#d04848";
 let passwordFiled;
-
+let count = 0;
+function isValidations(isValue) {
+  if (isValue) {
+    return count++;
+  }
+  console.log("isValue +>", isValue);
+  return isValue;
+}
 function handleErrorMessage(name, msg, input, i) {
   const isMsg = (msg, i) => {
     if (msg) {
@@ -23,42 +31,54 @@ function handleErrorMessage(name, msg, input, i) {
       break;
   }
 }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formGroup.forEach((forms) => {
+    const input = forms.querySelector("input");
+    const i = forms.querySelector("i");
+    i.style.background = "transparent";
+    input.addEventListener("input", () => {
+      let name = input.name,
+        isValid,
+        msg = null;
 
-formGroup.forEach((forms) => {
-  const input = forms.querySelector("input");
-  const i = forms.querySelector("i");
-  i.style.background = "transparent";
-  input.addEventListener("input", () => {
-    let name = input.name,
-      isValid,
-      msg = null;
-
-    try {
-      i.style.background = "#ff004ca1";
-      switch (name) {
-        case "username":
-          isValid = vfyjs.isUsername(input);
-          break;
-        case "email":
-          isValid = vfyjs.isEmail(input);
-          break;
-        case "password":
-          passwordFiled = input.value;
-          isValid = vfyjs.isPassword(input);
-          break;
-        case "passwordConf":
-          let Confirm = input.value;
-          isValid = Confirm === passwordFiled;
-          if (!msg) {
-            msg = "password do not match";
-          }
-          if (isValid && msg) {
-            msg = null;
-          }
+      try {
+        i.style.background = "#ff004ca1";
+        switch (name) {
+          case "username":
+            isValid = vfyjs.isUsername(input);
+            isValidations(isValid);
+            break;
+          case "email":
+            isValid = vfyjs.isEmail(input);
+            isValidations(isValid);
+            break;
+          case "password":
+            passwordFiled = input.value;
+            isValid = vfyjs.isPassword(input);
+            isValidations(isValid);
+            break;
+          case "passwordConf":
+            let Confirm = input.value;
+            isValid = Confirm === passwordFiled;
+            if (!msg) {
+              msg = "password do not match";
+            }
+            if (isValid && msg) {
+              msg = null;
+              count++;
+            }
+        }
+      } catch (error) {
+        msg = error.message;
       }
-    } catch (error) {
-      msg = error.message;
-    }
-    handleErrorMessage(name, msg, input, i);
+      handleErrorMessage(name, msg, input, i);
+    });
   });
+  console.log("count2 =>", count);
+  if (count >= 4) {
+    console.log("count =>", count);
+  } else {
+    alert("is not valued");
+  }
 });
