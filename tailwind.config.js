56182aa5455c99d978576f6c85f59fd10547ca6e
mahdi <mahdimamashli1383@gmail.com>
2024-03-02 13:31:07 +0330

    Update Tailwind config, remove unused CSS, update base template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      screens: {
+        mobile: "300px", // Mobile screens (default)
+        smallTb: "600px", // Small tablets
+        tablet: "900px", // Tablets
+        desktops: "1050px", // Desktops
+        large: "1536px", // Large desktops
+      },
+    },
   },
   plugins: [],
 };
