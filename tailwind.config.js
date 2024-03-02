37ee824d574bd91ca263fedbc1858430e1b4ef51
mahdi <mahdimamashli1383@gmail.com>
2024-03-02 13:15:12 +0330

    Update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{ejs}"],
+  content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
