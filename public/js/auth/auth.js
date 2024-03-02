e871e62898df4b971322bb07912217d9be5e006a
mahdi <mahdimamashli1383@gmail.com>
2024-03-02 14:29:11 +0330

    Update dependencies and add vfyjs

--- public/js/auth/auth.js
+++ public/js/auth/auth.js
@@ -1,5 +1,4 @@
-const password = document.getElementById('password')
-const ConfPassword = document.getElementById('ConfPassword')
-const registerForm = document.getElementById('registerForm')
-
-
+import { vfyjs } from "../lib/vfyjs/vfyjs.bundle";
+const password = document.getElementById("password");
+const ConfPassword = document.getElementById("ConfPassword");
+const registerForm = document.getElementById("registerForm");
