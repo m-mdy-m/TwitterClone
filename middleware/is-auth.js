9e785e2ae51179578bde970a9f48f1101ba2b03a
mahdi <mahdimamashli1383@gmail.com>
2024-03-03 23:39:27 +0330

    Remove console.log statement in ensureAuthenticated middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,5 @@
 exports.ensureAuthenticated = (req, res, next) => {
   if (req.session && req.session.user) {
-  console.log('test ensureAuthenticated')
   return next();
   }
   return res.status(401).redirect("/login");
