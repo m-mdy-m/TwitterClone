8427b120d6975863fa5056c780a7d8f5e66d60d6
mahdi <mahdimamashli1383@gmail.com>
2024-03-03 22:30:26 +0330

    Middleware: Add `test` middleware to the home route.

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,11 @@
 exports.ensureAuthenticated = (req, res, next) => {
   if (req.session && req.session.user) {
-    return next();
+  console.log('test ensureAuthenticated')
+  return next();
   }
   return res.status(401).redirect("/login");
 };
+exports.test = (req,res,nxt)=>{
+  console.log('test list middleware')
+  return nxt()
+}
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,11 @@
 const Xprz = require("xprz");
 const ensureAuthenticated = $read("middleware/is-auth").ensureAuthenticated;
+const test = $read("middleware/is-auth").test;
 const { Route } = new Xprz();
 const route = new Route();
 route
   .setRoute("/")
-  .using(ensureAuthenticated)
+  .using([ensureAuthenticated , test])
   .get(() => {
     const { status } = route.res();
     status(200).render("home", {
