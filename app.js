e19f62446c9dd2d28120d37865c344fee3614326
mahdi <mahdimamashli1383@gmail.com>
2024-03-03 10:48:55 +0330

    Add dotenv and connect to MongoDB.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // Importing the Xprz framework module
 const Xprz = require("xprz");
-const mongoose =$install('mongoose')
+const mongoose = $install("mongoose");
 // Creating a new instance of the Xprz framework
-const { App ,Package} = new Xprz();
-new Package().dotenv().setupDot()
+const { App, Package } = new Xprz();
+new Package().dotenv().setupDot();
 // Destructuring commonly used methods from the App instance
-const { launch, loadRoutes, setTemplateEngine, useJsonBody, static } = new App();
+const { launch, loadRoutes, setTemplateEngine, useJsonBody, static } =
+  new App();
 launch(); // Launching the server
 useJsonBody(); // Parsing JSON request bodies
 setTemplateEngine().ejs(); // Setting up the template engine (using EJS in this case)
-static('public') // Serving static files from the 'public' directory
+static("public"); // Serving static files from the 'public' directory
 loadRoutes("routes"); // Loading routes from the 'routes' directory
-// mongoose.connect()
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(console.log("connect to database"));
