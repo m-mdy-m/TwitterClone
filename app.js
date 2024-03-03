f437c06a2c70e112b722c71586a4c130ffba8bba
mahdi <mahdimamashli1383@gmail.com>
2024-03-03 10:38:05 +0330

    add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Importing the Xprz framework module
 const Xprz = require("xprz");
+const mongoose =$install('mongoose')
 // Creating a new instance of the Xprz framework
-const { App } = new Xprz();
+const { App ,Package} = new Xprz();
+new Package().dotenv().setupDot()
 // Destructuring commonly used methods from the App instance
 const { launch, loadRoutes, setTemplateEngine, useJsonBody, static } = new App();
 launch(); // Launching the server
@@ -9,3 +11,4 @@ useJsonBody(); // Parsing JSON request bodies
 setTemplateEngine().ejs(); // Setting up the template engine (using EJS in this case)
 static('public') // Serving static files from the 'public' directory
 loadRoutes("routes"); // Loading routes from the 'routes' directory
+// mongoose.connect()
