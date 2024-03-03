9648aa8f915ce154bbe09eb4edb3b97789986d3c
mahdi <mahdimamashli1383@gmail.com>
2024-03-03 11:06:09 +0330

    add comment

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const Xprz = require("xprz");
 // Creating a new instance of the Xprz framework
 const { App, Package } = new Xprz();
 new Package().dotenv().setupDot();
-$read("utils/database");
 // Destructuring commonly used methods from the App instance
 const { launch, loadRoutes, setTemplateEngine, useJsonBody, static } =
   new App();
@@ -12,3 +11,5 @@ useJsonBody(); // Parsing JSON request bodies
 setTemplateEngine().ejs(); // Setting up the template engine (using EJS in this case)
 static("public"); // Serving static files from the 'public' directory
 loadRoutes("routes"); // Loading routes from the 'routes' directory
+$read("utils/database"); // Loading database utility
+
