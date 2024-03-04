ba0c235fa45694d06516d90e3ad357558c0527e7
mahdi <mahdimamashli1383@gmail.com>
2024-03-04 06:26:25 +0330

    Update form validation and error handling logic

--- public/js/auth/validation.js
+++ public/js/auth/validation.js
@@ -5,59 +5,89 @@ const msgElm = document.getElementById("msgElm");
 handleFormValidation();
 export async function handleSubmit(e, submitUrl) {
   e.preventDefault();
+  // Ensure that form validation count meets the required threshold
   if (validationCount >= 4) {
     try {
+      // Collect form data
       const formData = new FormData(form);
+      // Convert form data to object
       const requestData = Object.fromEntries(formData.entries());
+      // Send form data to the server via POST request
       const response = await axios.post(submitUrl, requestData);
+      // Handle server response based on success or failure
       if (response.data.success) {
+        // If the server indicates success, handle accordingly
         handleSuccess(response.data.message);
       } else {
+        // If the server indicates failure, handle accordingly
         handleNotSuccess(response.data);
       }
     } catch (error) {
+      // Handle any errors that occur during the form submission process
       handleServerError(error);
     }
   }
 }
+// Function to handle unsuccessful form submission
 function handleNotSuccess(data) {
+  // Add error message styling to message element
   msgElm.classList.add("msg-errors");
+  // Extract error message from server response data
   const message = data.message;
-  displayMessage(msgElm, message, "#944E63"); // Clear previous error messages
+  // Display error message to the user
+  displayMessage(msgElm, message, "#944E63");
 }
 // Function to handle successful form submission
 function handleSuccess(message) {
+  // Reset the form to clear input fields
   form.reset();
+  // Add success message styling to message element
   msgElm.classList.add("msg-errors");
+  // Display success message to the user
   displayMessage(msgElm, message, "#90EE90");
+  // Redirect user to the home page after successful submission
   window.location.href = "/";
 }
 // Function to handle server errors
 function handleServerError(error) {
+  // Add error message styling to message element
   msgElm.classList.add("msg-errors");
-  msgElm.innerHTML = ""; // Clear previous error messages
+  // Clear previous error messages
+  msgElm.innerHTML = "";
+  // Initialize error message with a default value
   let errorMessage = "An error occurred";
+  // Check if the error is a server response error
   if (error.response) {
+    // Extract error message from server response data or use default message
     errorMessage = error.response.data.error || errorMessage;
+    // Extract form data from server response data, if available
     const formData = error.response.data.validationErrors;
+    // Update form values based on server response data, if available
     if (formData) {
       updateFormValues(formData);
     }
   } else if (error.request) {
+    // Handle cases where no response is received from the server
     errorMessage = "No response received from server";
   }
+  // Display error message to the user
   displayMessage(msgElm, errorMessage, "#fc6736");
 }
 // Function to display messages
 function displayMessage(element, message, color) {
+  // Set background color of message element
   element.style.background = color;
+  // Set message content of message element
   element.innerHTML = message;
 }
 
 // Function to update form values based on server response
 function updateFormValues(formData) {
+  // Iterate over form data entries
   for (const [name, value] of Object.entries(formData)) {
+    // Find input element with matching name attribute
     const input = form.querySelector(`[name="${name}"]`);
+    // If input element exists, update its value
     if (input) {
       input.value = value;
     }
