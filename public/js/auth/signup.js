34a3008e2a2cdecc88bdbe0430a0adcf9f1b6dec
mahdi <mahdimamashli1383@gmail.com>
2024-03-04 05:42:43 +0330

    Refactor signup and validation code

--- public/js/auth/signup.js
+++ public/js/auth/signup.js
@@ -1,75 +1,6 @@
-const mainUrl = "http://localhost:3000/";
-const formGroup = document.querySelectorAll(".form-group");
+import { handleFormValidation, validationCount } from "./utils";
 const form = document.getElementById("registerForm");
-const msgErrors = document.querySelectorAll(".msg-error");
-const validColor = "#90EE90";
-const invalidColor = "#d04848";
-let passwordField;
-let validationCount = 0;
-
-function incrementValidations(isValid) {
-  validationCount += isValid ? 1 : -1;
-}
-function handleErrorMessage(name, msg, input, errorMessageElement) {
-  const setBackgroundAndMessage = (color, message) => {
-    input.style.backgroundColor = color;
-    errorMessageElement.innerHTML = message;
-    if (!message) {
-      errorMessageElement.style.background = "transparent";
-    }
-  };
-  switch (name) {
-    case "username":
-    case "email":
-    case "password":
-    case "passwordConf":
-      setBackgroundAndMessage(msg ? invalidColor : validColor, msg);
-      break;
-  }
-}
-formGroup.forEach((forms) => {
-  const input = forms.querySelector("input");
-  input.value = "";
-  const errorMessageElement = forms.querySelector("i");
-  errorMessageElement.style.background = "transparent";
-  input.addEventListener("input", () => {
-    let name = input.name,
-      isValid,
-      errorMessage = null;
-
-    try {
-      errorMessageElement.style.background = "#ff004ca1";
-      switch (name) {
-        case "username":
-          isValid = vfyjs.isUsername(input);
-          incrementValidations(isValid);
-          break;
-        case "email":
-          isValid = vfyjs.isEmail(input);
-          incrementValidations(isValid);
-          break;
-        case "password":
-          passwordField = input.value;
-          isValid = vfyjs.isPassword(input);
-          incrementValidations(isValid);
-          break;
-        case "passwordConf":
-          let Confirm = input.value;
-          isValid = Confirm === passwordField;
-          if (!errorMessage) {
-            errorMessage = "password do not match";
-          }
-          if (isValid && errorMessage) {
-            errorMessage = null;
-            validationCount++;
-          }
-      }
-    } catch (error) {
-      errorMessage = error.message;
-    }
-    handleErrorMessage(name, errorMessage, input, errorMessageElement);
-  });
-});
+handleFormValidation();
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   if (validationCount >= 4) {
@@ -85,7 +16,7 @@ form.addEventListener("submit", async (e) => {
       // Parse JSON message and display its contents
       const message = response.data.message;
       msgSuccess.innerHTML = message; // Display JSON message
-      window.location.href = '/' // redirected
+      window.location.href = "/"; // redirected
     } catch (error) {
       const msgErrorServer = document.querySelector(".msg-error-fetch");
       msgErrorServer.style.background = "#fc6736";
