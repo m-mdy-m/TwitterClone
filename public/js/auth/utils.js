ae1e54fad29eb60c1836134b4b4860ac503af2f9
mahdi <mahdimamashli1383@gmail.com>
2024-03-04 05:30:01 +0330

    Update validation logic for username, email, password, and password confirmation.

--- public/js/auth/utils.js
+++ public/js/auth/utils.js
@@ -2,69 +2,80 @@ const validColor = "#90EE90";
 const invalidColor = "#d04848";
 const formGroup = document.querySelectorAll(".form-group");
 export let validationCount = 0;
+function validateInput(input, errorMessageElement) {
+  let name = input.name,
+    isValid,
+    errorMessage = null;
+
+  try {
+    errorMessageElement.style.background = "#ff004ca1";
+    switch (name) {
+      case "username":
+        isValid = vfyjs.isUsername(input);
+        incrementValidations(isValid);
+        break;
+      case "email":
+        isValid = vfyjs.isEmail(input);
+        incrementValidations(isValid);
+        break;
+      case "password":
+        passwordField = input.value;
+        isValid = vfyjs.isPassword(input);
+        incrementValidations(isValid);
+        break;
+      case "passwordConf":
+        let Confirm = input.value;
+        isValid = Confirm === passwordField;
+        if (!errorMessage) {
+          errorMessage = "password do not match";
+        }
+        if (isValid && errorMessage) {
+          errorMessage = null;
+          validationCount++;
+        }
+      default:
+        break;
+    }
+  } catch (error) {
+    errorMessage = error.message;
+  }
+  setBackgroundAndMessage(input,errorMessageElement,)
+  // handleErrorMessage(name, errorMessage, input, errorMessageElement,errorMessage ? invalidColor : validColor,);
+}
+
 function incrementValidations(isValid) {
   return (validationCount += isValid ? 1 : -1);
 }
-function handleErrorMessage(name, msg, input, errorMessageElement) {
-  const setBackgroundAndMessage = (color, message) => {
-    input.style.backgroundColor = color;
-    errorMessageElement.innerHTML = message;
-    if (!message) {
-      errorMessageElement.style.background = "transparent";
-    }
-  };
-  switch (name) {
-    case "username":
-    case "email":
-    case "password":
-    case "passwordConf":
-      setBackgroundAndMessage(msg ? invalidColor : validColor, msg);
-      break;
+function setBackgroundAndMessage(input, errorMessageElement, color, message) {
+  input.style.backgroundColor = color;
+  errorMessageElement.innerHTML = message;
+  if (!message) {
+    errorMessageElement.style.background = "transparent";
   }
 }
+// function handleErrorMessage(name, msg, input, errorMessageElement) {
+//   switch (name) {
+//     case "username":
+//     case "email":
+//     case "password":
+//     case "passwordConf":
+//       setBackgroundAndMessage(input,errorMessageElement,msg ? invalidColor : validColor, msg);
+//       break;
+//   }
+// }
 let passwordField;
+function handleInputEvent(event) {
+  const input = event.target;
+  const errorMessageElement = input.nextElementSibling;
+  errorMessageElement.style.background = "transparent";
+  validateInput(input, errorMessageElement);
+}
 export function handleFormValidation() {
   formGroup.forEach((forms) => {
     const input = forms.querySelector("input");
     input.value = "";
-    const errorMessageElement = forms.querySelector("i");
-    errorMessageElement.style.background = "transparent";
-    return input.addEventListener("input", () => {
-      let name = input.name,
-        isValid,
-        errorMessage = null;
-
-      try {
-        errorMessageElement.style.background = "#ff004ca1";
-        switch (name) {
-          case "username":
-            isValid = vfyjs.isUsername(input);
-            incrementValidations(isValid);
-            break;
-          case "email":
-            isValid = vfyjs.isEmail(input);
-            incrementValidations(isValid);
-            break;
-          case "password":
-            passwordField = input.value;
-            isValid = vfyjs.isPassword(input);
-            incrementValidations(isValid);
-            break;
-          case "passwordConf":
-            let Confirm = input.value;
-            isValid = Confirm === passwordField;
-            if (!errorMessage) {
-              errorMessage = "password do not match";
-            }
-            if (isValid && errorMessage) {
-              errorMessage = null;
-              validationCount++;
-            }
-        }
-      } catch (error) {
-        errorMessage = error.message;
-      }
-      handleErrorMessage(name, errorMessage, input, errorMessageElement);
-    });
+    // const errorMessageElement = forms.querySelector("i");
+    // errorMessageElement.style.background = "transparent";
+    input.addEventListener("input", handleInputEvent);
   });
 }
