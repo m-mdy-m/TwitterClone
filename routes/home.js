b3df9da713979d100fdc52055c5d48ad6d6c410f
mahdi <mahdimamashli1383@gmail.com>
2024-03-06 10:06:30 +0330

    Update route middleware to use an array instead of a single function.

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const { Route } = $read("utils/helper");
 const route = new Route();
 const redirectToHome = $read("middleware/redirectToHome");
 const { getHome } = $read("controller/home/home");
-route.setRoute("/home").using(redirectToHome).get(getHome);
+route.setRoute("/home").using([redirectToHome]).get(getHome);
 
 module.exports = route;
