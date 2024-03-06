1bdd565cb2565ccaebb89546b033ccc885980d26
mahdi <mahdimamashli1383@gmail.com>
2024-03-06 06:21:09 +0330

    Rename class `Xprz` to `HelperXprz`

--- utils/helper.js
+++ utils/helper.js
@@ -1,15 +1,24 @@
-class Xprz {
+class HelperXprz {
   constructor() {
+    // Require the 'xprz' module
     const xprz = require("xprz");
+
+    // Destructure the 'Route' class from the 'xprz' module
     const { Route } = new xprz();
+
+    // Assign the 'Route' class to the 'Route' property of this class instance
     this.Route = Route;
   }
+
   /**
-   * @class Route
-   * @returns 
+   * Returns the 'Route' class.
+   * @returns {Class} Route - The Route class from the 'xprz' module.
    */
   Route() {
+    // Returns the 'Route' class
     return this.Route;
   }
 }
-module.exports = new Xprz();
+
+// Export a new instance of the HelperXprz class
+module.exports = new HelperXprz();
