a0247973d0c5ca7c8e0da592de439188c32bf0b6
mahdi <mahdimamashli1383@gmail.com>
2024-03-06 22:35:56 +0330

    Update signup route and header link

--- public/js/auth/signup.js
+++ public/js/auth/signup.js
@@ -4,7 +4,7 @@ const form = document.getElementById("registerForm");
 // Define a wrapper function to dynamically pass the submit URL to handleSubmit
 function handleFormSubmit(event) {
   // Replace 'submitUrl' with the dynamic URL you want to use
-  const submitUrl = "/signup";
+  const submitUrl = "/auth/signup";
   handleSubmit(event, submitUrl);
 }
 
--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,5 +1,7 @@
 const { Route } = $read("utils/helper");
 const { getSignup, postSignup } = $read("controller/auth/signup");
 const route = new Route();
-route.setRoute("/signup").get(getSignup).post(postSignup);
+route.group("/auth", (r) => {
+    r.setRoute("/signup").get(getSignup).post(postSignup);
+});
 module.exports = route;
