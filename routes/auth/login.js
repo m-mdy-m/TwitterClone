c4fc9718621bfd4597bed81ec42c43c9830935c6
mahdi <mahdimamashli1383@gmail.com>
2024-03-06 22:53:47 +0330

    Refactor login and logout routes and add logout functionality.

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,11 +1,25 @@
 const { Route } = $read("utils/helper");
 const { getLogin, postLogin } = $read("controller/auth/login");
-const route= new Route();
+const route = new Route();
 route.group("/auth", (r) => {
   r.setRoute("/login").get(getLogin).post(postLogin);
-  r.setRoute("/logout").get(() => {
-    const { send } = r.res();
-    send("hi");
+  r.setRoute("/logout").post(() => {
+    const { getRes,getJsonHandler } = r.res();
+    const {success } = getJsonHandler()
+    const { getReq,error } = r.req();
+    const req = getReq(),
+      res = getRes();
+      if (req.session) {
+        req.session.destroy((err) => {
+          if (err) {
+            error(500,'Failed to delete session');
+          } else {
+            success('Session deleted successfully');
+          }
+        });
+      } else {
+        error(404,'User not logged in');
+      }
   });
 });
 
