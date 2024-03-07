865fe95b4d1818ab299395474a0dced7ac7fd014
mahdi <mahdimamashli1383@gmail.com>
2024-03-07 18:41:08 +0330

    Update header background color, add margin to navigation, update container padding, and add container screens.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,18 @@ module.exports = {
         container: " 15% minmax(85%, 1fr);",
       },
       backgroundColor: {
-        body: "#000000",
+        body: "#101011",
       },
       container: {
         center: true,
+        padding: 0,
+        screens: {
+          mobile: "300px", // Mobile screens (default)
+          smallTb: "600px", // Small tablets
+          tablet: "900px", // Tablets
+          desktops: "1050px", // Desktops
+          large: "1536px", // Large desktops
+        },
       },
       screens: {
         mobile: "300px", // Mobile screens (default)
