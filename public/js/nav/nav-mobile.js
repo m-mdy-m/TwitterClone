7b41d5ee2adf695d92196c923aec2c7614d0349f
mahdi <mahdimamashli1383@gmail.com>
2024-03-09 14:52:34 +0330

    Update mobile navigation CSS and JS

--- public/js/nav/nav-mobile.js
+++ public/js/nav/nav-mobile.js
@@ -3,18 +3,22 @@ const iconNavigation = document.getElementById("iconNavigation");
 const cancelIcon = document.getElementById("cancelIcon");
 // Add event listener to the navigation icon to open the mobile navigation
 iconNavigation.addEventListener("click", () => {
-    console.log('clicked');
-    navMobile.style.cssText=`
+  console.log("clicked");
+  navMobile.style.cssText = `
     width: 66.666667%;
     height:100%;
     display: block !important;
     transition: all 2s;
-    `
+    left:0;
+    `;
 });
 
 // Add event listener to the cancel icon to close the mobile navigation
 cancelIcon.addEventListener("click", () => {
-    navMobile.style.cssText=`
-    display: hidden;
-    `
+  navMobile.style.cssText = `
+    width: 0;
+    height:100%;
+    transition: all 2s;
+    left: -15rem;
+    `;
 });
