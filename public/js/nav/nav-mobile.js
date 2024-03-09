77d4e79c4428b02b5f94c790b2f08b72c212c60c
mahdi <mahdimamashli1383@gmail.com>
2024-03-09 15:14:20 +0330

    Fix mobile navigation CSS and JS

--- public/js/nav/nav-mobile.js
+++ public/js/nav/nav-mobile.js
@@ -1,11 +1,27 @@
 const navMobile = document.getElementById("nav-mobile");
 const iconNavigation = document.getElementById("iconNavigation");
 const cancelIcon = document.getElementById("cancelIcon");
+// Add event listener to the navigation icon to open the mobile navigation
 iconNavigation.addEventListener("click", () => {
-    navMobile.classList.add("show-nav-mobile");
+  navMobile.style.cssText = `
+    width: 66.666667%;
+    height:100%;
+    display: block !important;
+    transition: all 2s;
+    left:0;
+    `;
 });
 
+// Add event listener to the cancel icon to close the mobile navigation
 cancelIcon.addEventListener("click", () => {
-    navMobile.classList.remove("show-nav-mobile");
-    navMobile.classList.add("hide-nav-mobile");
+  navMobile.style.cssText = `
+    width: 0;
+    height:100%;
+    transition: all 2s;
+    left: -15rem;
+    `;
+  setTimeout(() => {
+    navMobile.style.display = "none";
+    navMobile.style.height = "0";
+  }, 2000);
 });
