b79153c12519e91269d70cf535e3dc84e3d4f459
mahdi <mahdimamashli1383@gmail.com>
2024-03-09 14:23:42 +0330

    Add mobile-specific classes for header width and alignment

--- public/js/nav/nav-mobile.js
+++ public/js/nav/nav-mobile.js
@@ -1,9 +1,16 @@
 const navMobile = document.getElementById("nav-mobile");
 const iconNavigation = document.getElementById("iconNavigation");
 const cancelIcon = document.getElementById("cancelIcon");
-cancelIcon.addEventListener("click", () => {
-  navMobile.style.cssText = 'display:none'
-});
+
+// Hide the mobile navigation initially
+navMobile.style.width = "0";
+
+// Add event listener to the navigation icon to open the mobile navigation
 iconNavigation.addEventListener("click", () => {
-    iconNavigation.style.cssText = 'display:flex'
+  navMobile.style.width = "100%";
+});
+
+// Add event listener to the cancel icon to close the mobile navigation
+cancelIcon.addEventListener("click", () => {
+  navMobile.style.width = "0";
 });
