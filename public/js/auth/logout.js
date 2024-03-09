70ce1e645836b891f8765483303f40c747810b0c
mahdi <mahdimamashli1383@gmail.com>
2024-03-09 16:40:20 +0330

    Update logout button and add CSRF token

--- public/js/auth/logout.js
+++ public/js/auth/logout.js
@@ -1,27 +1,30 @@
 import { displayMessage } from "./validation.js";
 const msgElm = document.getElementById("msgElm");
-async function handler(){
+async function handler(e) {
+  e.preventDefault()
   try {
     // Fetch CSRF token from a hidden input field in your HTML
     const csrfToken = document.querySelector('input[name="_csrf"]').value;
     // Include CSRF token in the request headers
     const headers = {
-      'X-CSRF-TOKEN': csrfToken
+      "X-CSRF-TOKEN": csrfToken,
     };
     const response = await axios.post("/auth/logout", {}, { headers });
     // Check if the response is successful
     if (response.status === 200 && response.data.success) {
-      window.location.href ='/auth/login'
+      window.location.href = "/auth/login";
       // Optionally, perform additional actions after logout
     } else {
-      msgElm.classList.add('msg-errors')
-      const message = response.data.message
-      displayMessage(msgElm,message,'#944E63')
+      msgElm.classList.add("msg-errors");
+      const message = response.data.message;
+      displayMessage(msgElm, message, "#944E63");
     }
   } catch (error) {
-    msgElm.classList.add('msg-errors')
+    msgElm.classList.add("msg-errors");
     msgElm.innerHTML = "";
-    const message = error.response.data.error
+    const message = error.response
+      ? error.response.data.error
+      : "An error occurred";
     displayMessage(msgElm, message, "#fc6736");
   }
 }
