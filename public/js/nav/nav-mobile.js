aaecb4f519d77294e807c149bcc0980de684164d
mahdi <mahdimamashli1383@gmail.com>
2024-03-09 17:14:45 +0330

    Update header styles, add mobile navigation, and fix navigation JS.

--- public/js/nav/nav-mobile.js
+++ public/js/nav/nav-mobile.js
@@ -1,14 +1,24 @@
 const navMobile = document.getElementById("nav-mobile");
 const iconNavigation = document.getElementById("iconNavigation");
 const cancelIcon = document.getElementById("cancelIcon");
-// Add event listener to the navigation icon to open the mobile navigation
-iconNavigation.addEventListener("click", () => {
-  navMobile.classList.remove("hide-nav");
-  navMobile.classList.add("show-nav");
-});
+if (window.innerWidth < 900) {
+  // Add event listener to the navigation icon to open the mobile navigation
+  iconNavigation.addEventListener("click", () => {
+      navMobile.classList.remove("hide-nav");
+      navMobile.classList.add("show-nav");
+  });
 
-// Add event listener to the cancel icon to close the mobile navigation
-cancelIcon.addEventListener("click", () => {
-  navMobile.classList.add("hide-nav");
-  navMobile.classList.remove("show-nav");
-});
+  // Add event listener to the cancel icon to close the mobile navigation
+  cancelIcon.addEventListener("click", () => {
+      navMobile.classList.add("hide-nav");
+      navMobile.classList.remove("show-nav");
+  });
+
+  // Add event listener to close the mobile navigation if clicked outside of it
+  document.addEventListener("click", (event) => {
+      if (!navMobile.contains(event.target) && !iconNavigation.contains(event.target)) {
+          navMobile.classList.add("hide-nav");
+          navMobile.classList.remove("show-nav");
+      }
+  });
+}
