fa97af4bd096725c8d64dbb2f1f88ec88e5e4ac9
mahdi <mahdimamashli1383@gmail.com>
2024-03-09 14:39:15 +0330

    Update header CSS, main CSS, nav-mobile JS, and end EJS file

--- public/js/nav/nav-mobile.js
+++ public/js/nav/nav-mobile.js
@@ -3,10 +3,11 @@ const iconNavigation = document.getElementById("iconNavigation");
 const cancelIcon = document.getElementById("cancelIcon");
 // Add event listener to the navigation icon to open the mobile navigation
 iconNavigation.addEventListener("click", () => {
-  navMobile.classList.add("show-nav-mobile");
+    console.log('clicked');
+  navMobile.classList.add("show-nav");
 });
 
 // Add event listener to the cancel icon to close the mobile navigation
 cancelIcon.addEventListener("click", () => {
-  navMobile.classList.remove("show-nav-mobile");
+  navMobile.classList.remove("show-nav");
 });
