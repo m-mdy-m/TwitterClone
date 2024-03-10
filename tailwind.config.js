324c6c4f6b3eefb4196910c63f5ad9b1a5d41a57
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 11:16:40 +0330

    Add a new component for reading tweets.

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
       gridTemplateRows: {
         home: "20% minmax(80% ,1fr)",
         tweet: "60% minmax(40%,1fr)",
+        readTweet : "30% minmax(70% , 1fr)"
       },
       backgroundColor: {
         body: "#131314",
