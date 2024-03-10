80d1151eee4cc64c62eaa02c2fc663ed7fc05167
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 23:56:53 +0330

    Add CSRF token endpoint and middleware.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // Importing the Xprz framework module
 const Xprz = require("xprz");
 // Creating a new instance of the Xprz framework
-const { App, Package } = new Xprz();
+const { App, Package, HttpMethod } = new Xprz();
 new Package().dotenv().setupDot();
 // Destructuring commonly used methods from the App instance
-const {use, launch, loadRoutes, setTemplateEngine, useJsonBody, static } = new App();
+const { use, launch, loadRoutes, setTemplateEngine, useJsonBody, static } = new App();
 launch(); // Launching the server
+const { setBaseRoute } = new HttpMethod();
 useJsonBody(); // Parsing JSON request bodies
 setTemplateEngine().ejs(); // set template engine ejs
 static("public"); // Serving static files from the 'public' directory
 $read("middleware/setup");
-use($read('middleware/user'))
-use($read('middleware/locals'))
+use($read("middleware/user"));
+use($read("middleware/locals"));
+setBaseRoute("/get-csrf-token").GET((req, res) => {
+  res.json({ csfToken: req.session.csrfSecret });
+});
 $read("utils/database"); // Loading database utility
 loadRoutes("routes"); // Loading routes from the 'routes' directory
