87004ce68cb08e9de3f2ce600a65484d688a10cb
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 16:00:48 +0330

    Remove unused JavaScript files.

---
+++ public/js/constants/handlers.js
@@ -0,0 +1,6 @@
+function handleFocus() {
+  document.getElementById("icon-tweet").style.opacity = "0";
+}
+function handleBlur() {
+  document.getElementById("icon-tweet").style.opacity = "100";
+}
--- public/js/main/main.js
+++
@@ -1,7 +0,0 @@
-function handleFocus() {
-    document.getElementById("icon-tweet").style.opacity = "0";
-  }
-  function handleBlur() {
-    document.getElementById("icon-tweet").style.opacity = "100";
-  }
-  
