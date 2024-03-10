2e01bbbbfe8b06e2f3c96cbf761af81eef400485
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 15:50:50 +0330

    Add event listeners to the tweet input field.

---
+++ public/js/main/main.js
@@ -0,0 +1,7 @@
+function handleFocus() {
+    document.getElementById("icon-tweet").style.opacity = "0";
+  }
+  function handleBlur() {
+    document.getElementById("icon-tweet").style.opacity = "100";
+  }
+  
