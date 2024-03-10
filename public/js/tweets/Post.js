f9fffaece786dad5d4652d28c30e29fd85b10085
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 21:08:21 +0330

    Add function to create a tweet.

--- public/js/tweets/Post.js
+++ public/js/tweets/Post.js
@@ -1,11 +1,19 @@
 import { displayMessage } from "../auth/validation.js";
+
+// Get the message element from the DOM
 const msgElm = document.getElementById("msgElm");
+
+// Function to create a tweet
 export async function createTweet(val) {
+  // If the tweet data is valid
   if (val.valid) {
+    // Hide the error message element
     msgElm.style.opacity = 0;
     msgElm.style.display = "none";
+    // Send a POST request to create the tweet
     const response = await axios.post("/tweets", val.value);
   } else {
+    // If the tweet data is invalid, display the error message
     msgElm.style.display = "block";
     displayMessage(msgElm, val.message, "#FF0000");
   }
