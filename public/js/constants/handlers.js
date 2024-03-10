2b3f94d54cd69cdefb5f22146995845a60eb7161
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 16:39:30 +0330

    Add keypress event listener to textarea

--- public/js/constants/handlers.js
+++ public/js/constants/handlers.js
@@ -1,3 +1,4 @@
+
 const iconElement = document.getElementById("icon-tweet");
 // Function to hide the icon when textarea is focused
 export function hideIconOnFocus() {
@@ -7,4 +8,12 @@ export function hideIconOnFocus() {
 // Function to show the icon when textarea loses focus
 export function showIconOnBlur() {
   iconElement.style.opacity = 1;
+}
+export function keypress(e){
+  const value = e.target.value
+  const trimmed = vfyjs.trimValue(value)
+  console.log('trimmed=>',trimmed);
+  const validator = vfyjs.inputValidations(trimmed)
+  const a = validator.hasAlphanumeric()
+  console.log('a=>',a);
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 // Importing handlers from a separate file
-import { hideIconOnFocus, showIconOnBlur } from "./Constants/handlers.js";
+import { hideIconOnFocus, keypress, showIconOnBlur } from "./Constants/handlers.js";
 
 // Get references to the textarea and the icon element
 const textarea = document.getElementById("tweetInput");
 // Add event listeners to handle focus and blur events
 textarea.addEventListener("focus", hideIconOnFocus);
 textarea.addEventListener("blur", showIconOnBlur);
+textarea.addEventListener('keypress',keypress)
