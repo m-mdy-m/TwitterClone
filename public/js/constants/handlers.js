85e7ec669f25ee8191964f9098bec8c2320e0bdd
mahdi <mahdimamashli1383@gmail.com>
2024-03-10 17:11:19 +0330

    Update CSS and JS files for tweet character count and textarea height

--- public/js/constants/handlers.js
+++ public/js/constants/handlers.js
@@ -1,4 +1,6 @@
 const iconElement = document.getElementById("icon-tweet");
+const charCount = document.getElementById("charCount");
+const maxLength = 380;
 // Function to hide the icon when textarea is focused
 export function hideIconOnFocus() {
   iconElement.style.opacity = 0;
@@ -13,20 +15,31 @@ export function validateInput(e) {
   const tweet = e.target.value;
   const validation = validateTweet(tweet);
   if (!validation.valid) {
-    
   }
 }
 export function validateTweet(tweet) {
   const trimmed = vfyjs.trimValue(tweet);
   const validator = vfyjs.inputValidations(trimmed);
-  const maxLength = validator.hasMaxLength(280);
-  console.log('mav =>',maxLength);
+  const max = validator.hasMaxLength(maxLength);
   // Check tweet length
-  if (!maxLength) {
+  if (!max) {
     return {
       valid: false,
-      message: `Tweet exceeds maximum length of ${280} characters`,
+      message: `Tweet exceeds maximum length of ${maxLength} characters`,
     };
   }
   return { valid: true, message: "Tweet is valid" };
+}
+export function updateCharCount(e) {
+  const currentLength = e.target.value.length;
+  const textarea = e.target
+  charCount.textContent = `${currentLength}/${maxLength}`;
+  // Change color if exceeding maximum limit
+  if (currentLength > maxLength) {
+    charCount.style.color = 'red';
+    textarea.style.cssText = 'border:1px solid red'
+  }else{
+    charCount.style.color = 'rgb(107, 114, 128)';
+    textarea.style.cssText = 'border:1px solid #343435'
+  }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 // Importing handlers from a separate file
-import { hideIconOnFocus, showIconOnBlur, validateInput } from "./Constants/handlers.js";
+import { hideIconOnFocus, showIconOnBlur, updateCharCount, validateInput } from "./Constants/handlers.js";
 
 // Get references to the textarea and the icon element
 const textarea = document.getElementById("tweetInput");
@@ -7,3 +7,4 @@ const textarea = document.getElementById("tweetInput");
 textarea.addEventListener("focus", hideIconOnFocus);
 textarea.addEventListener("blur", showIconOnBlur);
 textarea.addEventListener('keypress',validateInput)
+textarea.addEventListener('input', updateCharCount)
