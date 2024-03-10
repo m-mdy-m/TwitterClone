7046b2f18a80ce58ad6bcdddc7bcb951109d4b1b
mahdi <mahdimamashli1383@gmail.com>
2024-03-11 02:01:56 +0330

    Update CSRF token handling in tweet creation.

--- middleware/locals.js
+++ middleware/locals.js
@@ -1,5 +1,4 @@
 module.exports = (req, res, nxt) => {
   res.locals.isAuth = req.session.user;
-  res.locals.csrfToken = req.csrfToken();
   nxt();
 };
--- routes/tweet/create.js
+++ routes/tweet/create.js
@@ -6,22 +6,12 @@ router.setRoute("/tweets").post(() => {
   const body = getBody();
   const req = getReq();
   console.log("req : ", req);
-  // Retrieve CSRF token from the request headers
-  const csrfToken = req.headers["X-CSRF-Token"];
-
-  // Retrieve CSRF token from the user's session
-  const storedCsrfToken = req.session.csrfSecret;
-  console.log("CSRF Token from request:", csrfToken);
-  console.log("Stored CSRF Token:", storedCsrfToken);
-  // Verify CSRF token
-  if (csrfToken === storedCsrfToken) {
-    // CSRF token is valid, process the request
+  const csrfToken = req.csrfToken();
+  const clientCsrfToken  = req.headers["X-CSRF-Token"];
+  if (csrfToken === clientCsrfToken) {
     console.log("CSRF token is valid");
-    console.log("Body of tweets post request:", body);
   } else {
-    // CSRF token is invalid or missing, reject the request
     console.log("CSRF token is invalid or missing");
-    // Respond with an error status code or message
   }
 });
 
