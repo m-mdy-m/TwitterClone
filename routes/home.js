4de9d0f48cca32766d5ef47ef7b2f9b9343526b7
mahdi <mahdimamashli1383@gmail.com>
2024-03-11 12:15:12 +0330

    Update request and response variable names in home route.

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,8 @@ route.setRoute("/").get((req, res) => {
   res.redirect("/home");
 });
 route.setRoute("/home").get((req, res) => {
-  const { getReq } = req();
-  const { status } = res();
+  const { getReq } = req;
+  const { status } = res;
   const request = getReq();
   status(200).render("home", {
     Title: "Home",
