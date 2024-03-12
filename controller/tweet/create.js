37693f4ca24f838e4ad576f23d9e22fbf6f002f1
mahdi <mahdimamashli1383@gmail.com>
2024-03-12 13:53:51 +0330

    Changed the response status code from 200 to 201 and added a new error handler for internal server errors.

--- controller/tweet/create.js
+++ controller/tweet/create.js
@@ -1,20 +1,20 @@
 const PostTweet = require("../../model/PostTweet");
-exports.postTweet =  async (req, res) => {
+exports.postTweet = async (req, res) => {
   const body = req.getBody();
   const { getJsonHandler } = res;
-  const { badRequest } = getJsonHandler();
+  const { badRequest, created, internalServerError } = getJsonHandler();
   const content = body.tweet;
   if (!content) {
     return badRequest("Tweet content is missing.");
   }
-  const data ={
-    content : content,
-    postedBy : req.user._id, 
-  }
+  const data = {
+    content: content,
+    postedBy: req.user._id,
+  };
   try {
     const post = await PostTweet.create(data);
-    res.json({ success: true, message: "Tweet posted successfully.", post });
+    created(post);
   } catch (error) {
-    res.status(500).json({ success: false, error: "Internal server error. Unable to post the tweet." });
+    internalServerError("Internal server error. Unable to post the tweet.");
   }
 };
