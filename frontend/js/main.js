0d0c39575454899ae656d9b5fb792cdb0c2c5352
mahdi <mahdimamashli1383@gmail.com>
2024-03-12 17:25:39 +0330

    Update frontend code to match backend changes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,11 @@
 // Importing handlers from a separate file
 import { hideIconOnFocus, showIconOnBlur, updateCharCount } from "./Constants/handlers.js";
+import { renderNav } from "./templates/navigation.js";
 import { fetchTweets } from "./tweets/Get.js";
 import { createTweet } from "./tweets/Post.js";
+const navMenu = document.getElementById('nav-menu')
 fetchTweets()
+renderNav(false,navMenu)
 const tweetButton = document.getElementById("tweetButton");
 const textarea = document.getElementById("tweetInput");
 // Get references to the textarea and the icon element
