b028f87172f781631020dc305cd716d989e5b39c
mahdi <mahdimamashli1383@gmail.com>
2024-03-12 15:29:11 +0330

    add api get tweets

--- controller/tweet/tweets.js
+++ controller/tweet/tweets.js
@@ -36,3 +36,7 @@ exports.postTweet = async (req, { getJsonHandler }) => {
     internalServerError("Internal server error. Unable to post the tweet.");
   }
 };
+
+exports.getTweets = async (req, res) => {
+  const tweets = await PostTweet.find();
+};
---
+++ routes/tweet/tweets.js
@@ -0,0 +1,7 @@
+const Xprz = require("xprz");
+const { Route } = new Xprz();
+const { postTweet, getTweets } = $read("controller/tweet/tweets");
+const router = new Route();
+router.setRoute("/tweets").post(postTweet).get(getTweets);
+
+module.exports = router;
