8df0c6c60aab855842940031bdd9298ad1f9a3d5
mahdi <mahdimamashli1383@gmail.com>
2024-03-12 19:08:56 +0330

    Add console.log statements to log tweets and errors.

--- backend/controller/home/home.js
+++ backend/controller/home/home.js
@@ -1,15 +1,16 @@
 const PostTweet = require("../../model/PostTweet");
+const path = require("path");
 exports.getHome = async (req, res) => {
   try {
     // Fetch tweets from the database
     const tweets = await PostTweet.find();
-    console.log("tweets =>", tweets);
     // Send JSON response with success true and tweet data
     res.json({
       success: true,
       data: tweets,
     });
   } catch (error) {
+    console.log("error =>", error);
     // If an error occurs, send an error response
     res.status(500).json({
       success: false,
