7013affe6b651b80b9290d7f7bea68315065ca0b
mahdi <mahdimamashli1383@gmail.com>
2024-03-12 13:46:22 +0330

    PostTweet Schema

---
+++ model/PostTweet.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+const PostTweet = new Schema(
+  {
+    Tweet: {
+      type: String,
+      trim: true,
+    },
+    postedBy : {
+        type : Schema.ObjectId,
+        ref : "User",
+        pinned : Boolean,
+    }
+  },
+  { timestamps: true }
+);
+module.exports = model("Tweet", PostTweet);
