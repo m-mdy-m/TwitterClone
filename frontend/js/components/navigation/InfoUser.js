5e25e9551e2db59ef7567bcf97804c4e2eafe439
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 13:03:14 +0330

    Refactor code for user info section in navigation bar.

--- frontend/js/components/navigation/InfoUser.js
+++ frontend/js/components/navigation/InfoUser.js
@@ -1,21 +1,13 @@
-export function InfoUser(){
+import { UserInfo } from "./gen/info.js";
 
-    return `<!-- info user -->
+export function InfoUser() {
+  const userStats = [
+    { id: "posts", value: 300, unit: "", label: "Posts" },
+    { id: "followers", value: 182, unit: "K", label: "Posts" },
+    { id: "following", value: 1.04, unit: "M", label: "Posts" },
+  ];
+  return `<!-- info user -->
     <div id="userInfo" class="flex items-center justify-center mobile:mt-1 mobile:gap-0 smallTb:mt-2 smallTb:gap-2 border-b-4 border-[#2f2f30]">
-        <!-- Posts -->
-        <div id="posts" class="text-center text-white rounded-lg tablet:p-4 mobile:p-1 smallTb:p-2">
-            <span class="header__navigation-info-user">300<i></i></span>
-            <p class="header__navigation-info-user-text">Posts</p>
-        </div>
-        <!-- Followers -->
-        <div id="followers" class="text-center text-white rounded-lg tablet:p-4 mobile:p-1 smallTb:p-2">
-            <span class="header__navigation-info-user">182<i>K</i></span>
-            <p class="header__navigation-info-user-text">Followers</p>
-        </div>
-        <!-- Following -->
-        <div id="following" class="text-center text-white rounded-lg tablet:p-4 mobile:p-1 smallTb:p-2">
-            <span class="header__navigation-info-user">1.04<i>M</i></span>
-            <p class="header__navigation-info-user-text">Following</p>
-        </div>
-    </div>`
+        ${UserInfo(userStats)}
+    </div>`;
 }
--- frontend/js/components/navigation/mobile.js
+++ frontend/js/components/navigation/mobile.js
@@ -1,3 +1,5 @@
+import { InfoUser } from "./InfoUser.js";
+
 export function MobileNavigation({ isAuth }) {
   const authTemplateMobile = `
     <!-- Profile User Section -->
@@ -13,24 +15,7 @@ export function MobileNavigation({ isAuth }) {
             <!-- Bio -->
             <p id="bio" class="text-center mt-2 text-gray-400 mobile:text-sm smallTb:text-base overflow-hidden whitespace-nowrap overflow-ellipsis">'Greatness، of small steps'!</p>
         </div>
-        <!-- info user -->
-        <div id="userInfo" class="flex items-center justify-center mobile:mt-1 mobile:gap-0 smallTb:mt-2 smallTb:gap-2 border-b-4 border-[#2f2f30]">
-            <!-- Posts -->
-            <div id="posts" class="text-center text-white rounded-lg tablet:p-4 mobile:p-1 smallTb:p-2">
-                <span class="header__navigation-info-user">300<i></i></span>
-                <p class="header__navigation-info-user-text">Posts</p>
-            </div>
-            <!-- Followers -->
-            <div id="followers" class="text-center text-white rounded-lg tablet:p-4 mobile:p-1 smallTb:p-2">
-                <span class="header__navigation-info-user">182<i>K</i></span>
-                <p class="header__navigation-info-user-text">Followers</p>
-            </div>
-            <!-- Following -->
-            <div id="following" class="text-center text-white rounded-lg tablet:p-4 mobile:p-1 smallTb:p-2">
-                <span class="header__navigation-info-user">1.04<i>M</i></span>
-                <p class="header__navigation-info-user-text">Following</p>
-            </div>
-        </div>
+        ${InfoUser()}
     </div>
 `;
 
