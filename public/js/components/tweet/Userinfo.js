52f4cfc722c77e92f947c4eccb00a2e9081d8f49
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 20:47:35 +0330

    Changed the timestamp field from `currentTimeFormatted` to `createdAt`.

--- public/js/components/tweet/Userinfo.js
+++ public/js/components/tweet/Userinfo.js
@@ -1,6 +1,6 @@
 import { UserProfile } from "./UserProfile.js";
 
-export function UserInfo({ username, profile, currentTimeFormatted }) {
+export function UserInfo({ username, profile, createdAt }) {
   return `
       <div class="flex justify-start items-center">
          ${UserProfile({username:username,profile:profile,className : "w-8 h-8"})}  
@@ -9,7 +9,7 @@ export function UserInfo({ username, profile, currentTimeFormatted }) {
                   <h2 class="username font-Aktiv__regular text-xs text-white">${username}</h2>
               </div>
               <div class="flex justify-center items-center text-gray-500">
-                  <p class="flex items-center space-x-1 font-mavis__bold text-sm">${currentTimeFormatted}</p>
+                  <p class="flex items-center space-x-1 font-mavis__bold text-sm">${createdAt}</p>
               </div>
           </div>
       </div>
