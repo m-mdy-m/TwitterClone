584912961225ebedcccfa215b0e2e549b550cf80
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 11:34:01 +0330

    add story components

---
+++ frontend/js/components/home/Stroies.js
@@ -0,0 +1,15 @@
+import { StoryBox } from "./StoryBox";
+import { UserStory } from "./UserStory";
+
+export function WrapperStory({username,profilePic}){
+    return `
+    <!-- Your Add Story -->
+    <div class="flex justify-start items-center w-auto mr-2">
+      ${StoryBox()}
+    </div>
+    <!-- Wrapper Story -->
+    <div class="flex justify-start items-center w-auto mr-2">
+        ${UserStory({username,profilePic})}
+    </div>
+    `
+}
