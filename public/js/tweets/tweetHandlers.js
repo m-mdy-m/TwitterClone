fa2aabb4218ade7d59d40998979d6ae4877470b5
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 21:01:21 +0330

    Refactor tweet rendering and handling functions.

--- public/js/tweets/tweetHandlers.js
+++ public/js/tweets/tweetHandlers.js
@@ -1,52 +1,39 @@
 import Tweet from "../components/Tweet.js";
 const wrapper = document.getElementById("wrapperTweet");
+
 export function AddTweet(response) {
-  const responseData = response.data;
-  const tweetData = responseData.data;
-  const content = tweetData.content;
-  const { username, profilePic } = tweetData.postedBy;
-  const createdAt = getCurrentTimeFormatted(tweetData.createdAt);
-  const tweetTemplate = Tweet({
-    username,
-    content,
-    profile: profilePic,
-    createdAt,
-  });
-  wrapper.insertAdjacentHTML("beforeend", tweetTemplate);
-  document.getElementById("tweetInput").value = "";
+  const tweetData = response.data.data;
+  const tweetTemplate = renderTweet(tweetData);
+  appendTweet(tweetTemplate);
+  clearTweetInput();
 }
+
 export function ShowTweets(response) {
   const tweets = response.data.tweets;
   tweets.forEach((tweet) => {
-    const content = tweet.content;
-    const { username, profilePic } = tweet.postedBy;
-    const createdAt = getCurrentTimeFormatted(tweet.createdAt);
-    const tweetTemplate = Tweet({
-      username,
-      content,
-      profile: profilePic,
-      createdAt,
-    });
-    wrapper.insertAdjacentHTML("beforeend", tweetTemplate);
-    document.getElementById("tweetInput").value = "";
+    const tweetTemplate = renderTweet(tweet);
+    appendTweet(tweetTemplate);
   });
+  clearTweetInput();
 }
 
-function getCurrentTimeFormatted(Time) {
-  // Extracting the time when the post was created
-  const createdAt = new Date(Time);
+function renderTweet(tweet) {
+  const { username, profilePic, content, createdAt } = tweet;
+  const formattedCreatedAt = getCurrentTimeFormatted(createdAt);
+  return Tweet({ username, profile: profilePic, content, createdAt: formattedCreatedAt });
+}
 
-  // Format the date as "DD Mon" (e.g., "13 Mar")
-  const formattedDate = createdAt.toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "short",
-  });
-  // Format the time as "hh:mm AM/PM" (e.g., "8:35 PM")
-  const formattedTime = createdAt.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  });
-  // Displaying the formatted timestamp
+function appendTweet(tweetTemplate) {
+  wrapper.insertAdjacentHTML("beforeend", tweetTemplate);
+}
+
+function clearTweetInput() {
+  document.getElementById("tweetInput").value = "";
+}
+
+function getCurrentTimeFormatted(time) {
+  const createdAt = new Date(time);
+  const formattedDate = createdAt.toLocaleDateString("en-US", { day: "numeric", month: "short" });
+  const formattedTime = createdAt.toLocaleTimeString("en-US", { hour: "numeric", minute: "2-digit", hour12: true });
   return formattedDate + " • " + formattedTime;
 }
