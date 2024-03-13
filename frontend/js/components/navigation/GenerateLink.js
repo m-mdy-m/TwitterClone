4666d613b2178f59481bbf7eed5ded77d43a17b2
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 12:43:06 +0330

    Replace hard-coded navigation links with a function that dynamically generates them.

---
+++ frontend/js/components/navigation/GenerateLink.js
@@ -0,0 +1,12 @@
+export function GenerateLink([links]) {
+  let template = "";
+  links.forEach((href) => {
+    template += `
+    <!-- ${href} Link -->
+  <a href="/${href}" class="header__navigation-link flex items-center space-x-4 mobile:space-x-2 smallTb:space-x-3 transition duration-300 transform hover:scale-105 hover:bg-[#232627]">
+      <img src="/assets/icon/nav/${href}.svg" alt="${href}" class="tablet:images__icons-nav mobile:w-7 mobile:h-7 smallTb:w-8 smallTb:h-8">
+      <h2 class="text-lg font-semibold text-white capitalize mobile:text-base smallTb:text-lg">${href}</h2>
+  </a>`;
+  });
+  return template;
+}
