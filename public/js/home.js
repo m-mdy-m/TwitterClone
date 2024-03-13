0c44c9848a20611548b21df419190e5655d6ddf5
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 19:59:38 +0330

    Added event listeners for focus and blur events to the textarea element, and added a click event listener to the tweet button. Also imported the necessary handlers from the handlers.js file.

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,20 @@
 // Importing handlers from a separate file
 import { fetchTweets } from "./common/helper.js";
-import { hideIconOnFocus, showIconOnBlur, updateCharCount } from "./common/handlers.js";
+import {
+  hideIconOnFocus,
+  showIconOnBlur,
+  updateCharCount,
+} from "./common/handlers.js";
 import { createTweet } from "./tweets/Post.js";
-console.log('hi');
-fetchTweets()
+console.log("hi");
+fetchTweets();
 const tweetButton = document.getElementById("tweetButton");
 const textarea = document.getElementById("tweetInput");
 // Get references to the textarea and the icon element
-tweetButton.addEventListener("click", () => createTweet(updateCharCount({ target: textarea })));
+tweetButton.addEventListener("click", () =>
+  createTweet(updateCharCount({ target: textarea }))
+);
 // Add event listeners to handle focus and blur events
 textarea.addEventListener("focus", hideIconOnFocus);
 textarea.addEventListener("blur", showIconOnBlur);
-textarea.addEventListener('input',updateCharCount)
+textarea.addEventListener("input", updateCharCount);
