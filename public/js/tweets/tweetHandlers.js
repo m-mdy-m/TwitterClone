b2814f9942a961c8046e475a3874a29762ceb87a
mahdi <mahdimamashli1383@gmail.com>
2024-03-13 21:07:30 +0330

    Update tweet rendering function to use 'postedBy' instead of 'username' and 'profilePic'

--- public/js/tweets/tweetHandlers.js
+++ public/js/tweets/tweetHandlers.js
@@ -36,8 +36,8 @@ export function ShowTweets(response) {
 // Function to render a single tweet template
 function renderTweet(tweet) {
   // Extract relevant data from the tweet object
-  const { username, profilePic, content, createdAt } = tweet;
-
+  const { postedBy, content, createdAt } = tweet;
+  const { username, profilePic } = postedBy;
   // Format the creation time of the tweet
   const formattedCreatedAt = getCurrentTimeFormatted(createdAt);
 
