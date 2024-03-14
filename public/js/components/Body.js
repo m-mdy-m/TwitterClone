52902ce3935fcad8305d14f8d10c21fb2bf3980c
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 12:37:43 +0330

    Update class names in Body, BodyContent, and Form components

--- public/js/components/Body.js
+++ public/js/components/Body.js
@@ -1,13 +1,13 @@
 import Header from "./common/header.js";
 import { mainContent } from "./mainContent.js";
 
-export function BodyContent({ isAuth, profile, username }) {
+export function BodyContent({ isAuth, profile, username,classMain }) {
   return `
 <div class="container mobile:m-0 tablet:px-4 h-screen grid mobile:grid-cols-1 tablet:grid-cols-container">
     <header class="flex flex-col items-center tablet:h-auto justify-start mobile:pl-2 mobile:py-1 tablet:m-4 mobile:absolute tablet:relative">
     ${Header({ isAuth, profile, username })}    
     </header>
-    <main  class="grid grid-cols-main mt-4 mr-4 mb-4 bg-[#19191a] rounded-lg border border-[#28282a] relative">
+    <main  class="${classMain} mt-4 mr-4 mb-4 bg-[#19191a] rounded-lg border border-[#28282a] relative">
     <div id="msgElm" class="absolute top-4 left-[25%] opacity-0 transform text-white px-4 py-2 rounded-md shadow-lg transition-opacity duration-300 pointer-events-none z-50"></div>
         ${mainContent()} 
     </main>
