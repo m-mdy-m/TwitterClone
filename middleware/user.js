956d8f330605466d12c9e86cff5c06ccbcd0d626
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 23:57:46 +0330

    add commend

--- middleware/user.js
+++ middleware/user.js
@@ -1,4 +1,5 @@
 const User = require("../model/User");
+// Middleware function to fetch user data from the database based on session information
 module.exports = async (req, res, nxt) => {
     if (req.session.user) {
     const user = await User.findById(req.session.user._id);
