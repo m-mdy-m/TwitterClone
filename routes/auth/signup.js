5c2ec5e0a0a3c76fc8cc6eb4308a1e8bb00af606
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 23:51:55 +0330

    add comment

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,7 +1,10 @@
 const { Route } = $read("utils/helper");
 const { getSignup, postSignup } = $read("controller/auth/signup");
 const route = new Route();
+// Grouping authentication-related routes under the "/auth" prefix
 route.group("/auth", (r) => {
+    // Defining a GET route for the signup page and a POST route for signup action
     r.setRoute("/signup").get(getSignup).post(postSignup);
 });
+
 module.exports = route;
