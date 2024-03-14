996b5146f6e9600edbae997300f5a71aa9eefd56
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 23:54:56 +0330

    update comment

--- controller/home/home.js
+++ controller/home/home.js
@@ -1,10 +1,11 @@
 const path = require("path");
+// Controller function to handle requests for the home page
 exports.getHome = async (req, { status, sendFile }) => {
   try {
-    // Send the HTML file to the client
+    // Send the HTML file for the home page to the client
     sendFile(path.join(process.cwd(), "/public/views/home.html"));
   } catch (error) {
-    // If an error occurs, send an error response
+    // If an error occurs while sending the file, send an error response
     status(500).json({
       success: false,
       error: "Failed to fetch tweets",
