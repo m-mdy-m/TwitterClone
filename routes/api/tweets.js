a32b0c49fef422827554b283d239e94e025755a3
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 23:49:22 +0330

    add comment

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -2,10 +2,17 @@ const Xprz = require("xprz");
 const { Route } = new Xprz();
 const { postTweet, getTweets } = $read("controller/tweet/tweets");
 const router = new Route();
+// Grouping API routes under the "/api" prefix
 router.group("/api", (r) => {
+  // Defining a POST route for creating a new tweet
   r.setRoute("/create").post(postTweet);
+
+  // Defining a GET route for fetching tweets
   r.setRoute("/tweets").get(getTweets);
+
+  // Defining a PUT route for liking a tweet
   r.setRoute("/like").put((req, res) => {
+    // Responding with a JSON message
     res.status(200).json({ message: "hi" });
   });
 });
