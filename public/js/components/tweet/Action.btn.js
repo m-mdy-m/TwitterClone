3649708be31b430ca14ee88b37041b080eca6ec9
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 18:14:49 +0330

    Update the text for the comment button from 'Chat' to 'Comment'

--- public/js/components/tweet/Action.btn.js
+++ public/js/components/tweet/Action.btn.js
@@ -3,7 +3,7 @@ import { Icon } from "./Icon.js";
 export function ActionButtons() {
   return `
       <div class="flex w-full justify-start items-center pt-3 px-2 text-white [&>*]:cursor-pointer">
-         ${Icon({src:'nav/chat-comment.svg',alt: 'Chat',className:'flex items-center '})}
+         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center '})}
          ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center '})}
          ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center '})}
          ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center '})}
--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,6 +1,6 @@
-export function Icon({ src, alt, className = "" }) {
+export function Icon({ src, alt, className = "",id="" }) {
   return `
-<div class="${className} mr-4 icon-container transform transition duration-150 hover:scale-110">
+<div id="${id}" class="${className} mr-4 icon-container transform transition duration-150 hover:scale-110">
     <img src="/assets/icon/${src}" alt="${alt}">
   </div>`;
 }
