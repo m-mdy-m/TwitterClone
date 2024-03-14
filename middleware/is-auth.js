a3d25c11395796f79e130c6c68a1c5ca4e321bf8
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 23:56:36 +0330

    add commit

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,8 @@
+// Middleware function to ensure user authentication
 exports.ensureAuthenticated = (req, res, next) => {
+  // Check if the session exists and if the user object is stored in the session
   if (req.session && req.session.user) {
-  return next();
+    return next();
   }
   return res.status(401).redirect("/auth/login");
 };
