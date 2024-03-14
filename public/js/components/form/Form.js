2cb094b72139602395d8f8be76a5f1ccf7e28f1b
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 13:01:06 +0330

    Update CSS padding and margin styles and add a new class. Update JS for AuthLink and Form components.

--- public/js/components/form/Form.js
+++ public/js/components/form/Form.js
@@ -1,6 +1,6 @@
 export function Form({ title, action, method, children }) {
   return `
-    <form id="registerForm" action="${action}" method="${method}" class="flex-col flex justify-center items-center bg-gray-800 py-4 px-12 rounded-lg">
+    <form id="registerForm" action="${action}" method="${method}" class="flex-col flex justify-center items-center bg-gray-800 py-7 px-12 rounded-lg">
       <h1 class="text-3xl text-center mb-4 text-gray-300">${title}</h1>
       ${children}
     </form>
