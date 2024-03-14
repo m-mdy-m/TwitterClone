7dfaa35a6a9a7ebd96ec427198d2ee48d3a94add
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 12:33:59 +0330

    Wrap the form component with a div to center it.

--- public/js/components/form/Form.js
+++ public/js/components/form/Form.js
@@ -1,8 +1,10 @@
 export function Form({ title, action, method, children }) {
   return `
-      <form id="registerForm" action="${action}" method="${method}" class="flex-col flex justify-center items-center bg-gray-800 p-4 rounded-lg">
-        <h1 class="text-3xl text-center mb-4">${title}</h1>
-        ${children}
-      </form>
+  <div class="flex justify-center items-center">
+    <form id="registerForm" action="${action}" method="${method}" class="flex-col flex justify-center items-center bg-gray-800 p-4 rounded-lg">
+      <h1 class="text-3xl text-center mb-4">${title}</h1>
+      ${children}
+    </form>
+  </div>
     `;
 }
