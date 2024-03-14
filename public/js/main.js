ec39f031d97d3af21ba1ac1c73382e7bd2d59291
mahdi <mahdimamashli1383@gmail.com>
2024-03-14 12:34:43 +0330

    Remove console.log

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@ import { mainContent } from "./components/mainContent.js";
 const isAuth = localStorage.getItem("logged");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
-console.log('=>',isAuth);
 const routes = {
   isAuth: isAuth,
   profile: "/assets/images/profilePic.png",
