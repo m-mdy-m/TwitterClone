6c4d80111de93a67c0b702a4a9691c2669a93e71
mahdi <mahdimamashli1383@gmail.com>
2024-03-15 19:53:40 +0330

    Added height and width classes, added 8px font size class, added a red notification bubble to the icon component.

--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,6 +1,8 @@
 export function Icon({ src, alt, className = "", id = "" }) {
   return `
-  <div class="${className} icons mr-4 icon-container transform transition duration-150 hover:scale-110">
+    <div class="${className} relative icons mr-4 icon-container transform transition duration-150 hover:scale-110">
       <img src="/assets/icon/${src}" alt="${alt}" id="${id}">
-    </div>`;
+      <p class="absolute top-0 right-0 h-4 w-4 text-white rounded-full flex items-center justify-center text-[8px]">20</p>
+    </div>
+    `;
 }
