15f17e570104d6f34888220a39907da6802c418b
mahdi <mahdimamashli1383@gmail.com>
2024-03-15 20:32:33 +0330

    Update CSS and JS files for tweet icons.

--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -2,7 +2,7 @@ export function Icon({ src, alt, className = "", id = "",styles="",display='hidd
   return `
     <div class="${className} relative flex justify-center icons mr-4 icon-container transform transition duration-150">
       <img src="/assets/icon/${src}" alt="${alt}" id="${id}" class="${styles}">
-      <p class="${display} absolute -bottom-2 -right-3 h-4 w-4  text-[#5c6065] rounded-full flex items-center justify-center text-[8px]">20</p>
+      <p class="${display} absolute -bottom-2 -right-2 text-[#5c6065] rounded-full flex items-center justify-center text-[8px]">999</p>
     </div>
     `;
 }
