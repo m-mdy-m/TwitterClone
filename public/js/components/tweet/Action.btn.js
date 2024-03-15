0b2f613e9b22cd6eee6778839516469f2af66891
mahdi <mahdimamashli1383@gmail.com>
2024-03-15 20:49:17 +0330

    Change the size of the icons in the tweet action buttons.

--- public/js/components/tweet/Action.btn.js
+++ public/js/components/tweet/Action.btn.js
@@ -1,12 +1,12 @@
 import { Icon } from "./Icon.js";
-const styleIcon ='w-6 h-6'
+const styleIcon ='w-4 h-4'
 export function ActionButtons(countLike,countReTweet) {
   return `
       <div class="flex w-full justify-start items-center pt-3 px-2 text-white [&>*]:cursor-pointer">
-         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center' , id: 'commentIcon',})}
-         ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center' , id: 'likeIcon',display:'block',count:countLike})}
-         ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center' , id: 'retweetIcon',display:'block',count:countReTweet})}
-         ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center ', id: 'shareIcon',})}
+         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center',styleIcon , id: 'commentIcon',})}
+         ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center',styleIcon , id: 'likeIcon',display:'block',count:countLike})}
+         ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center',styleIcon , id: 'retweetIcon',display:'block',count:countReTweet})}
+         ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center ',styleIcon, id: 'shareIcon',})}
       </div>
     `;
 }
