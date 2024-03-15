0999cd37e43465101c439b87f78b2e848b7366b0
mahdi <mahdimamashli1383@gmail.com>
2024-03-15 20:09:57 +0330

    Update CSS class names and add styles to Tweet action buttons and icons

--- public/js/components/tweet/Action.btn.js
+++ public/js/components/tweet/Action.btn.js
@@ -3,10 +3,10 @@ import { Icon } from "./Icon.js";
 export function ActionButtons() {
   return `
       <div class="flex w-full justify-start items-center pt-3 px-2 text-white [&>*]:cursor-pointer">
-         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center' , id: 'commentIcon'})}
-         ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center' , id: 'likeIcon'})}
-         ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center' , id: 'retweetIcon'})}
-         ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center ', id: 'shareIcon'})}
+         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center' , id: 'commentIcon',styles:'w-5 h-5',display:'block'})}
+         ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center' , id: 'likeIcon',styles:'w-5 h-5',display:'block'})}
+         ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center' , id: 'retweetIcon',styles:'w-5 h-5',display:'block'})}
+         ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center ', id: 'shareIcon',styles:'w-5 h-5',display:'block'})}
       </div>
     `;
 }
--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,8 +1,8 @@
-export function Icon({ src, alt, className = "", id = "" }) {
+export function Icon({ src, alt, className = "", id = "",styles="",display='hidden' }) {
   return `
-    <div class="${className} relative icons mr-4 icon-container transform transition duration-150 hover:scale-110">
-      <img src="/assets/icon/${src}" alt="${alt}" id="${id}">
-      <p class="absolute top-0 right-0 h-4 w-4 text-white rounded-full flex items-center justify-center text-[8px]">20</p>
+    <div class="${className} flex justify-center icons mr-4 icon-container transform transition duration-150 hover:scale-110">
+      <img src="/assets/icon/${src}" alt="${alt}" id="${id}" class="${styles}">
+      <p class="${display} h-5 w-5 text-white rounded-full flex items-center justify-center text-[8px]">20</p>
     </div>
     `;
 }
