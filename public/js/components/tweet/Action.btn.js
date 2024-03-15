b9fbec509a2e5bd9802633be1723ee841a976d22
mahdi <mahdimamashli1383@gmail.com>
2024-03-15 20:19:44 +0330

    Update icon sizes and remove unused CSS class

--- public/js/components/tweet/Action.btn.js
+++ public/js/components/tweet/Action.btn.js
@@ -3,10 +3,10 @@ import { Icon } from "./Icon.js";
 export function ActionButtons() {
   return `
       <div class="flex w-full justify-start items-center pt-3 px-2 text-white [&>*]:cursor-pointer">
-         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center' , id: 'commentIcon',styles:'w-5 h-5',display:'block'})}
-         ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center' , id: 'likeIcon',styles:'w-5 h-5',display:'block'})}
-         ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center' , id: 'retweetIcon',styles:'w-5 h-5',display:'block'})}
-         ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center ', id: 'shareIcon',styles:'w-5 h-5',display:'block'})}
+         ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center' , id: 'commentIcon',styles:'w-6 h-6'})}
+         ${Icon({src:'nav/heart-null.svg',alt: 'Like',className:'flex items-center' , id: 'likeIcon',styles:'w-6 h-6',display:'block'})}
+         ${Icon({src:'nav/ReTweet.svg',alt: 'ReTweet',className:'flex items-center' , id: 'retweetIcon',styles:'w-6 h-6',display:'block'})}
+         ${Icon({src:'nav/share.svg',alt: 'share',className:'flex items-center ', id: 'shareIcon',styles:'w-6 h-6'})}
       </div>
     `;
 }
--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,8 +1,8 @@
 export function Icon({ src, alt, className = "", id = "",styles="",display='hidden' }) {
   return `
-    <div class="${className} flex justify-center icons mr-4 icon-container transform transition duration-150 hover:scale-110">
+    <div class="${className} flex justify-center icons mr-4 icon-container transform transition duration-150">
       <img src="/assets/icon/${src}" alt="${alt}" id="${id}" class="${styles}">
-      <p class="${display} h-5 w-5 text-white rounded-full flex items-center justify-center text-[8px]">20</p>
+      <p class="${display} text-[#5c6065] rounded-full flex items-center justify-center text-xs h-4 w-4">20</p>
     </div>
     `;
 }
