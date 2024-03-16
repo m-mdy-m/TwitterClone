8d5f1b920c80caca11281a2a9c89b3e62d874489
mahdi <mahdimamashli1383@gmail.com>
2024-03-17 02:14:42 +0330

    Add authentication middleware to tweet and user routes

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,16 +1,17 @@
 const Xprz = require("xprz");
 const { Route } = new Xprz();
+const { ensureAuthenticated,verifyToken } = $read("middleware/is-auth")
 const { postTweet, getTweets,putLike } = $read("controller/tweet/tweets");
 const router = new Route();
 // Grouping API routes under the "/api" prefix
 router.group("/api", (r) => {
   // Defining a POST route for creating a new tweet
-  r.setRoute("/create").post(postTweet);
+  r.setRoute("/create").using([ensureAuthenticated,verifyToken]).post(postTweet);
 
   // Defining a GET route for fetching tweets
-  r.setRoute("/tweets").get(getTweets);
+  r.setRoute("/tweets").using([ensureAuthenticated,verifyToken]).get(getTweets);
 
   // Defining a PUT route for liking a tweet
-  r.setRoute("/like/:id").put(putLike);
+  r.setRoute("/like/:id").using([ensureAuthenticated,verifyToken]).put(putLike);
 });
 module.exports = router;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,11 @@
 const Xprz = require("xprz");
 const { Route } = new Xprz();
+const { ensureAuthenticated,verifyToken } = $read("middleware/is-auth")
 const { findUser } = $read("controller/users/user");
 const router = new Route();
 // Grouping API routes under the "/api" prefix
 router.group("/api", (r) => {
   // Defining a GET route for fetching tweets
-  r.setRoute("/user/:username").get(findUser);
+  r.setRoute("/user/:username").using([ensureAuthenticated,verifyToken]).get(findUser);
 });
 module.exports = router;
