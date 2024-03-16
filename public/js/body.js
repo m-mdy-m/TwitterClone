d35d40aa45863117cab34098ff937a24b76ecf90
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 16:33:11 +0330

    Update routes and components to support authentication.

--- public/js/body.js
+++ public/js/body.js
@@ -3,19 +3,16 @@ const isAuth = localStorage.getItem("logged");
 // Define routes and corresponding HTML content
 const routes = {
   "/home": {
+    isAuth,
     classMain: "grid grid-cols-main",
     classMsgError: "absolute top-4 left-[25%]",
     height: "full",
   },
   "/auth/signup": {
-    classMain: "flex justify-center items-center",
-    classMsgError: "absolute top-4 left-[50%]",
-    height: "screen",
+    isAuth,
   },
   "/auth/login": {
-    classMain: "flex justify-center items-center",
-    classMsgError: "absolute top-4 left-[50%]",
-    height: "screen",
+    isAuth,
   },
 };
 const path = window.location.pathname;
--- public/js/components/Body.js
+++ public/js/components/Body.js
@@ -1,11 +1,11 @@
 import Header from "./common/header.js";
 import { mainContent } from "./mainContent.js";
 
-export function BodyContent({classMain,classMsgError,height}) {
+export function BodyContent({isAuth,profile,username,classMain='flex justify-center items-center',classMsgError='absolute top-4 left-[50%]',height='screen'}={}) {
   return `
   <div class="container mobile:m-0 tablet:px-4 h-${height}  grid mobile:grid-cols-1 tablet:grid-cols-container">
     <header class="flex flex-col items-center tablet:h-auto justify-start mobile:pl-2 mobile:py-1 tablet:m-4 mobile:absolute tablet:relative">
-    ${Header()}    
+    ${Header({isAuth,profile,username})}    
     </header>
     <main  class="${classMain} mt-4 mr-4 mb-4 bg-[#19191a] rounded-lg border border-[#28282a] relative">
     <div id="msgElm" class="${classMsgError} opacity-0 transform text-white px-4 py-2 rounded-md shadow-lg transition-opacity duration-300 pointer-events-none z-50"></div>
