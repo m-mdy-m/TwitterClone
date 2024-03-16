cf19fd50f5a14d63254b1dff8e42a7421327947f
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 12:56:15 +0330

    Update like button based on fetched count. Handle fetch errors.

--- public/js/tweets/Like.js
+++ public/js/tweets/Like.js
@@ -2,14 +2,23 @@ import { fetchLike } from "../common/fetchTweet.js";
 import { getId } from "../common/handlers.js";
 export async function handleClick(event) {
   const elm = event.target;
-  const id = getId(elm);
-  const count = await fetchLike(id);
-  
-  const pTag = elm.nextElementSibling;
-  pTag.innerHTML = count;
-  if (count > 0) {
-    elm.src = '/assets/icon/nav/heart-full.svg'
-  }else{
-    elm.src = '/assets/icon/nav/heart-null.svg'
+  try {
+    const id = getId(elm);
+    const count = await fetchLike(id);
+
+    // Update the like button based on the fetched count
+    const pTag = elm.nextElementSibling;
+    if (count > 0) {
+      pTag.innerHTML = count;
+      elm.src = "/assets/icon/nav/heart-full.svg";
+    } else {
+      pTag.innerHTML = null;
+      elm.src = "/assets/icon/nav/heart-null.svg";
+    }
+  } catch (error) {
+    // Handle fetch error
+    console.error("Error fetching like count:", error);
+    // Revert back to the default heart icon
+    elm.src = "/assets/icon/nav/heart-null.svg";
   }
 }
