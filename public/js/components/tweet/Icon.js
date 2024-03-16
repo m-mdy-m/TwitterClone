b5ee22f3494c0b47137a3877d3efae4041d51c6d
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 13:05:06 +0330

    Added like count to the icon and updated the CSS class name for the heart animation.

--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,8 +1,8 @@
-export function Icon({ src, alt, className =  "", id = "",styleIcon="",display='hidden'}) {
+export function Icon({ src, alt, className =  "", id = "",styleIcon="",display='hidden',count=''}) {
   return `
     <div class="${className} relative flex justify-center icons mr-4 icon-container transform transition duration-150 hover:scale-105">
       <img src="/assets/icon/${src}" alt="${alt}" id="${id}" class="${styleIcon}">
-      <p class="${display} absolute -bottom-1 -right-1 text-[#5c6065] rounded-full flex items-center justify-center text-[10px]"></p>
+      <p class="${display} absolute -bottom-1 -right-1 text-[#5c6065] rounded-full flex items-center justify-center text-[10px]">${count}</p>
     </div>
     `;
 }
