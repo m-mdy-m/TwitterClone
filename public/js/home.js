86dc0608fd852205585ed2cd1e37ebf81ad0314e
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 19:37:59 +0330

    Update code to only allow logged-in users to create tweets.

--- public/js/home.js
+++ public/js/home.js
@@ -2,16 +2,19 @@ import { fetchTweets } from "./common/fetchTweet.js";
 import { hideIconOnFocus, showIconOnBlur, updateCharCount } from "./common/handlers.js";
 import { onDocumentLoad } from "./nav/navigation-handler.js";
 import { createTweet } from "./tweets/Post.js";
+const isAuth = localStorage.getItem('logged')
 document.addEventListener("DOMContentLoaded", async ()=>{
     onDocumentLoad()
     const tweetButton = document.getElementById("tweetButton");
-    const textarea = document.getElementById("tweetInput");
-    // Get references to the textarea and the icon element
-    tweetButton.addEventListener("click", () => createTweet(updateCharCount({ target: textarea })));
-    // Add event listeners to handle focus and blur events
-    textarea.addEventListener("focus", hideIconOnFocus);
-    textarea.addEventListener("blur", showIconOnBlur);
-    textarea.addEventListener('input',updateCharCount)
+    if (isAuth && tweetButton) {
+        const textarea = document.getElementById("tweetInput");
+        // Get references to the textarea and the icon element
+        tweetButton.addEventListener("click", () => createTweet(updateCharCount({ target: textarea })));
+        // Add event listeners to handle focus and blur events
+        textarea.addEventListener("focus", hideIconOnFocus);
+        textarea.addEventListener("blur", showIconOnBlur);
+        textarea.addEventListener('input',updateCharCount)
+    }
     await fetchTweets()
 });
 
