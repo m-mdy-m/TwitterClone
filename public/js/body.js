70974f803051b2e2c21e10ab1baae44b19c194ef
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 16:34:42 +0330

    Revise code to remove references to `isAuth` variable.

--- public/js/body.js
+++ public/js/body.js
@@ -1,18 +1,10 @@
 import { BodyContent } from "./components/Body.js";
-const isAuth = localStorage.getItem("logged");
 // Define routes and corresponding HTML content
 const routes = {
   "/home": {
-    isAuth,
     classMain: "grid grid-cols-main",
     classMsgError: "absolute top-4 left-[25%]",
     height: "full",
-  },
-  "/auth/signup": {
-    isAuth,
-  },
-  "/auth/login": {
-    isAuth,
   },
 };
 const path = window.location.pathname;
