e9385775c3704e1b24be265fada47ebdaf775583
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 12:37:38 +0330

    Update CSS styles and JS components for tweet action buttons and icons

--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,8 +1,8 @@
-export function Icon({ src, alt, className =  "", id = "",styleIcon="",display='hidden',count='' }) {
+export function Icon({ src, alt, className =  "", id = "",styleIcon="",display='hidden'}) {
   return `
     <div class="${className} relative flex justify-center icons mr-4 icon-container transform transition duration-150 hover:scale-105">
       <img src="/assets/icon/${src}" alt="${alt}" id="${id}" class="${styleIcon}">
-      <p class="${display} absolute -bottom-1 -right-2 text-[#5c6065] rounded-full flex items-center justify-center text-[8px]">${count}</p>
+      <p class="${display} absolute -bottom-1 -right-2 text-[#5c6065] rounded-full flex items-center justify-center text-[8px]"></p>
     </div>
     `;
 }
