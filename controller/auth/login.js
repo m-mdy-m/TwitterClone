a587dfa98c53cecd1bc47e4bd0fed6274a3f46ac
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 21:12:27 +0330

    Update user login details.

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -12,18 +12,24 @@ exports.getLogin = (req, { sendFile }) => {
  *  username : Ads242sxz
  * email : mahdimamashli1383@gmail.com
  * password : Asd24242@4
- * 
- * : user 2 
- * 
+ *
+ * : user 2
+ *
  * username : Mahdi1383
  * email : sdma@gmail.com
  * password : Mdy_mmshly1383
+ *
+ * :user 3
+ * username : Medishn2
+ * email : Asdd@gmail.com
+ * password : Mdy_mmshly1393@@
  */
 // Controller function to handle login form submission
 exports.postLogin = async (req, { status, getJsonHandler }) => {
   const { getBody } = req;
   const { username, email, password } = getBody();
-  const { badRequest, notFound, internalServerError,updated } = getJsonHandler();
+  const { badRequest, notFound, internalServerError, updated } =
+    getJsonHandler();
   try {
     // Validate if username or email is provided
     if (!username && !email) {
@@ -57,7 +63,7 @@ exports.postLogin = async (req, { status, getJsonHandler }) => {
     // Set user session
     req.session.user = user;
     // Send success response
-    return updated(user)
+    return updated(user);
   } catch (error) {
     internalServerError("Internal server error. Please try again later.");
   }
