e95a6b4247452ed464cfae74f2d0c3855b41ea87
mahdi <mahdimamashli1383@gmail.com>
2024-03-17 03:06:09 +0330

    Add authentication middleware to the home route.

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -3,4 +3,12 @@ const Xprz = require("xprz");
 const { Package } = new Xprz();
 const { jwt } = new Package();
 
+// Middleware function to ensure user authentication
+exports.ensureAuthenticated = (req, res, next) => {
+  // Check if the session exists and if the user object is stored in the session
+  if (req.session && req.session.token) {
+    return next();
+  }
+  return res.status(401).redirect("/auth/login");
+};
 exports.verifyToken = jwt().jwtAuthenticate(process.env.JWT_SECRET);
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
 const { Route } = $read("utils/HelperXprz");
 const route = new Route();
-const { verifyToken } = $read("middleware/is-auth")
+const { ensureAuthenticated } = $read("middleware/is-auth")
 const { getHome } = $read("controller/home/home");
 route.setRoute("/").get((req, { redirect }) => redirect("/home"));
-route.setRoute("/home").using([verifyToken]).get(getHome);
+route.setRoute("/home").using([ensureAuthenticated]).get(getHome);
 
 module.exports = route;
