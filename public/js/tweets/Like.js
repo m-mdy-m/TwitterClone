5e47250dd7e092320dc0519a26a9bd8b60ffcd12
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 12:32:04 +0330

    Update Like.js to fetch and display like count.

--- public/js/tweets/Like.js
+++ public/js/tweets/Like.js
@@ -1,11 +1,9 @@
 import { fetchLike } from "../common/fetchTweet.js";
 import { getId } from "../common/handlers.js";
-import { ActionButtons } from "../components/tweet/Action.btn.js";
-
-// Handles the click event on icons.
 export async function handleClick(event) {
-  const elm = event.target
+  const elm = event.target;
   const id = getId(elm);
   const count = await fetchLike(id);
-  console.log('elm +>',elm);
-} 
+  const pTag = elm.nextElementSibling;
+  pTag.innerHTML = count;
+}
