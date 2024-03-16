a094439e8a5a4c54ba7411099d586ddf2b9c03b9
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 20:56:18 +0330

    Update API operations and helper functions

--- public/js/utils/apiOperations.js
+++ public/js/utils/apiOperations.js
@@ -1,21 +1,35 @@
-import { AddTweet, ShowTweets, attachIconClickListeners } from "../tweets/tweetHandlers";
-import { getCSRFHeader, handleNotSuccess, handleSuccess, showMessage } from "./helper";
-import { clearAuth,clearWelcomePhotoFlag, getMsgElement, setItem } from "./utils";
-const msgElm = getMsgElement()
+import {
+  AddTweet,
+  ShowTweets,
+  attachIconClickListeners,
+} from "../tweets/tweetHandlers";
+import {
+  getCSRFHeader,
+  handleNotSuccess,
+  handleSuccess,
+  showMessage,
+} from "./helper";
+import {
+  clearAuth,
+  clearWelcomePhotoFlag,
+  getMsgElement,
+  setItem,
+} from "./utils";
+const msgElm = getMsgElement();
 // Function to Signup or Login user
 export async function authenticateUser(url, requestData, header, form) {
   const response = await axios.post(url, requestData, header);
   // Handle server response based on success or failure
   if (response.data.success) {
-      const user = response.data.data;
-      // If the server indicates success, handle accordingly
-      handleSuccess(form, response.data.message);
-      // Set the 'showWelcomePhoto' flag to 'true' in localStorage
-      setItem("showWelcomePhoto", response.data.success);
-      setItem("logged", response.data.success);
+    const user = response.data.data;
+    // If the server indicates success, handle accordingly
+    handleSuccess(form, response.data.message);
+    // Set the 'showWelcomePhoto' flag to 'true' in localStorage
+    setItem("showWelcomePhoto", response.data.success);
+    setItem("logged", response.data.success);
   } else {
-      // If the server indicates failure, handle accordingly
-      handleNotSuccess(response.data);
+    // If the server indicates failure, handle accordingly
+    handleNotSuccess(response.data);
   }
 }
 // Function to handle user logout
@@ -24,48 +38,48 @@ export async function logoutUser(header) {
   const logoutResponse = await axios.post("/auth/logout", {}, header);
   // Check if the logout request is successful
   if (logoutResponse.status === 200 && logoutResponse.data.success) {
-      // Redirect the user to the login page
-      window.location.href = "/auth/login";
-      // Clear localStorage flags
-      clearWelcomePhotoFlag();
-      clearAuth();
+    // Redirect the user to the login page
+    window.location.href = "/auth/login";
+    // Clear localStorage flags
+    clearWelcomePhotoFlag();
+    clearAuth();
   } else {
-      // If the logout process fails, display an error message
-      const message = logoutResponse.data.message;
-      showMessage(msgElm, message, "#944E63");
+    // If the logout process fails, display an error message
+    const message = logoutResponse.data.message;
+    showMessage(msgElm, message, "#944E63");
   }
 }
 // Function to fetch tweets from the API
 export async function getTweets() {
   try {
-      // Make a GET request to fetch tweets
-      const response = await axios.get("/api/tweets");
-      // Check if the request was successful
-      if (response.data.success) {
-          // Display the fetched tweets
-          ShowTweets(response);
-          // Handle displaying tweets on the UI as needed
-          attachIconClickListeners();
-      } else {
-          // Display error message with error-related color
-          showMessage(msgElm, response.data.error, "#ff6347"); // Error color
-      }
+    // Make a GET request to fetch tweets
+    const response = await axios.get("/api/tweets");
+    // Check if the request was successful
+    if (response.data.success) {
+      // Display the fetched tweets
+      ShowTweets(response);
+      // Handle displaying tweets on the UI as needed
+      attachIconClickListeners();
+    } else {
+      // Display error message with error-related color
+      showMessage(msgElm, response.data.error, "#ff6347"); // Error color
+    }
   } catch (error) {
-      // Handle errors
-      if (error.response && error.response.data && error.response.data.error) {
-          // Display error message returned from the server
-          showMessage(msgElm, error.response.data.error, "#ffd700");
-      } else {
-          // Display a generic error message for other errors
-          showMessage(
-              msgElm,
-              "An unexpected error occurred while creating the tweet. Please try again later.",
-              "#cc0000"
-          );
-      }
+    // Handle errors
+    if (error.response && error.response.data && error.response.data.error) {
+      // Display error message returned from the server
+      showMessage(msgElm, error.response.data.error, "#ffd700");
+    } else {
+      // Display a generic error message for other errors
+      showMessage(
+        msgElm,
+        "An unexpected error occurred while creating the tweet. Please try again later.",
+        "#cc0000"
+      );
+    }
   }
 }
-// Fetch CSRF header and send a POST request to create a tweet
+// Function to create a tweet
 export async function tweetCreation(data) {
   // Get CSRF header
   const header = await getCSRFHeader();
@@ -79,14 +93,15 @@ export async function tweetCreation(data) {
     showMessage(msgElm, response.data.error, "ffd700");
   }
 }
+// Function to toggle like on a tweet
 export async function toggleLike(id) {
   try {
-    const header = await getCSRFHeader();
-    const response = await axios.put(`/api/like/${id}`, {}, header);
-    console.log("response =>", response);
-    const countLike = response.data.data.likes.length;
-    return countLike;
+      const header = await getCSRFHeader();
+      const response = await axios.put(`/api/like/${id}`, {}, header);
+      console.log("response =>", response);
+      const countLike = response.data.data.likes.length;
+      return countLike;
   } catch (error) {
-    console.log("error =>", error);
+      console.log("error =>", error);
   }
 }
