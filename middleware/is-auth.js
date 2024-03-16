a2c6943c9f0db3a2c32ccd0d49da6f2ab05329d8
mahdi <mahdimamashli1383@gmail.com>
2024-03-17 02:13:35 +0330

    Added JWT authentication to the home route.

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,3 +1,8 @@
+const Xprz = require("xprz");
+
+const {Package }= new Xprz()
+const { jwt} = new Package()
+
 // Middleware function to ensure user authentication
 exports.ensureAuthenticated = (req, res, next) => {
   // Check if the session exists and if the user object is stored in the session
@@ -6,3 +11,25 @@ exports.ensureAuthenticated = (req, res, next) => {
   }
   return res.status(401).redirect("/auth/login");
 };
+exports.verifyToken  = (req,res,nxt)=>{
+  // Extract the token from the request headers or query parameters
+  const token = req.headers.authorization;
+
+  // Check if the token exists
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized: Token missing' });
+  }
+  try {
+    // Verify the token
+    const decoded = jwt().jwtVerify(token, process.env.JWT_SECRET);
+
+    // Attach the decoded token payload to the request object for further use
+    req.user = decoded;
+
+    // Proceed to the next middleware or route handler
+    nxt();
+  } catch (error) {
+    // Handle token verification errors
+    return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+  }
+}
