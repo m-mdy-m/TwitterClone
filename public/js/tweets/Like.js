252968f322f5b7a587cb3d6259ba4059eb662465
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 12:53:07 +0330

    Update like button to change color when clicked

--- public/js/tweets/Like.js
+++ public/js/tweets/Like.js
@@ -4,6 +4,12 @@ export async function handleClick(event) {
   const elm = event.target;
   const id = getId(elm);
   const count = await fetchLike(id);
+  
   const pTag = elm.nextElementSibling;
   pTag.innerHTML = count;
+  if (count > 0) {
+    elm.src = '/assets/icon/nav/heart-full.svg'
+  }else{
+    elm.src = '/assets/icon/nav/heart-null.svg'
+  }
 }
