66be9ada07e9206c8915364776c590d4e8c33b32
mahdi <mahdimamashli1383@gmail.com>
2024-03-16 19:41:17 +0330

    Update event listeners and add fetch tweets on page load.

--- public/js/home.js
+++ public/js/home.js
@@ -1,21 +1,28 @@
 import { fetchTweets } from "./common/fetchTweet.js";
-import { hideIconOnFocus, showIconOnBlur, updateCharCount } from "./common/handlers.js";
+import {
+  hideIconOnFocus,
+  showIconOnBlur,
+  updateCharCount,
+} from "./common/handlers.js";
 import { onDocumentLoad } from "./nav/navigation-handler.js";
 import { createTweet } from "./tweets/Post.js";
-const isAuth = localStorage.getItem('logged')
-document.addEventListener("DOMContentLoaded", async ()=>{
-    onDocumentLoad()
-    const tweetButton = document.getElementById("tweetButton");
-    if (isAuth && tweetButton) {
-        const textarea = document.getElementById("tweetInput");
-        // Get references to the textarea and the icon element
-        tweetButton.addEventListener("click", () => createTweet(updateCharCount({ target: textarea })));
-        // Add event listeners to handle focus and blur events
-        textarea.addEventListener("focus", hideIconOnFocus);
-        textarea.addEventListener("blur", showIconOnBlur);
-        textarea.addEventListener('input',updateCharCount)
-    }
-    await fetchTweets()
+const isAuth = localStorage.getItem("logged");
+const path = window.location.pathname;
+document.addEventListener("DOMContentLoaded", async () => {
+  onDocumentLoad();
+  const tweetButton = document.getElementById("tweetButton");
+  if (isAuth && tweetButton) {
+    const textarea = document.getElementById("tweetInput");
+    // Get references to the textarea and the icon element
+    tweetButton.addEventListener("click", () =>
+      createTweet(updateCharCount({ target: textarea }))
+    );
+    // Add event listeners to handle focus and blur events
+    textarea.addEventListener("focus", hideIconOnFocus);
+    textarea.addEventListener("blur", showIconOnBlur);
+    textarea.addEventListener("input", updateCharCount);
+  }
+  if (path !== "/auth/signup" && path !== "/auth/login") {
+    await fetchTweets();
+  }
 });
-
-
