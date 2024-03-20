0a656b4f2fa5705f2a6e4ecd4da97cc8502a8ee8
mahdi <mahdimamashli1383@gmail.com>
2024-03-21 01:36:14 +0330

    Adds a Database class that automatically connects to the MongoDB database when an instance of the class is created.

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,15 @@
+// Import the Mongoose library
 const mongoose = require("mongoose");
+
+// Define a Database class
 class Database {
-  constructor() {
-    this.connect();
-  }
-  connect() {
+  // Method to establish a connection to the MongoDB database
+  static connect() {
     mongoose
-      .connect(process.env.MONGODB_URI)
-      .then(console.log("connect to database"));
+      .connect(process.env.MONGODB_URI) // Connect to the MongoDB URI specified in the environment variables
+      .then(console.log("Connected to the database")); // Log a message indicating successful connection
   }
 }
-module.exports = new Database();
+
+// Export an instance of the Database class
+module.exports =Database;
