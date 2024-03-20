11d228dc121d70b38091dff7d140de0f088336e3
mahdi <mahdimamashli1383@gmail.com>
2024-03-20 09:05:27 +0330

    Changed the way the router is created from `new Route()` to `Xprz.Route()`.

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,7 @@
 const Xprz = require("xprz");
-const { Route } = new Xprz();
 const { verifyToken } = $read("middleware/is-auth");
 const { findUser } = $read("controller/users/user");
-const router = new Route();
+const router = Xprz.Route()
 // Defining a GET route for fetching tweets
 router.route("/user-info").using([verifyToken]).get(findUser);
 module.exports = router;
