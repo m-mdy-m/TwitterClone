f2c0b63c4b4d784368a407eacbbbd26b57408ef2
mahdi <mahdimamashli1383@gmail.com>
2024-03-20 10:53:23 +0330

    Remove `verifyToken` middleware from tweet routes.

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -4,16 +4,17 @@ const { verifyToken } = $read("middleware/is-auth");
 const { postTweet, getTweets, likeTweet, retweet } = $read(
   "controller/tweet/tweets"
 );
+route.globalMiddleware([verifyToken])
 // Grouping API routes under the "/api" prefix
 route.group("/api", (r) => {
   // Defining a POST route for creating a new tweet
-  r.route("/create").using([verifyToken]).post(postTweet);
+  r.route("/create").post(postTweet);
 
   // Defining a GET route for fetching tweets
-  r.route("/tweets").using([verifyToken]).get(getTweets);
+  r.route("/tweets").get(getTweets);
 
   // Defining a PUT route for liking a tweet
-  r.route("/like/:id").using([verifyToken]).put(likeTweet);
-  r.route("/retweet/:id").using([verifyToken]).put(retweet);
+  r.route("/like/:id").put(likeTweet);
+  r.route("/retweet/:id").put(retweet);
 });
 module.exports = route;
