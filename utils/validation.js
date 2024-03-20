920eedc4ce597e0dd99f0fa0b244d39b29f87cd9
mahdi <mahdimamashli1383@gmail.com>
2024-03-21 01:32:24 +0330

    Update API operations and validation functions.

--- utils/validation.js
+++ utils/validation.js
@@ -1,3 +1,4 @@
+// Define a function to validate authentication input
 function validateAuth(req, validationFailed) {
   // Define validation rules
   const rules = {
@@ -6,7 +7,10 @@ function validateAuth(req, validationFailed) {
     password: "password",
     passwordConf: "same:password",
   };
+
+  // Verify user input against defined rules
   const errors = req.verifyBody(rules);
+
   // If there are validation errors, respond with failure
   if (Object.keys(errors).length > 0) {
     validationFailed({ errors });
@@ -15,4 +19,6 @@ function validateAuth(req, validationFailed) {
 
   return true; // Return true to indicate validation success
 }
-module.exports = {validateAuth}
+
+// Export the validateAuth function for reuse in other modules
+module.exports = { validateAuth };
