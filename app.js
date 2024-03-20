df019119c955d0242087bdae27c43a42535bdc8b
mahdi <mahdimamashli1383@gmail.com>
2024-03-20 08:58:57 +0330

    Changed the import of the Xprz library from 'xprz' to "xprz".

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const Xprz = require('xprz')
+const Xprz = require("xprz");
 
 const { App, Package } = new Xprz();
-new Package().dotenv().setupDot(); 
+new Package().dotenv().setupDot();
 const { use, launch, loadRoutes, useJsonBody, static } = new App();
 launch();
-useJsonBody(); 
+useJsonBody();
 static("public");
 const cookieParser = $install("cookie-parser");
 use(cookieParser());
 $read("middleware/setup");
-$read("utils/database"); 
+$read("utils/database");
 loadRoutes("routes");
