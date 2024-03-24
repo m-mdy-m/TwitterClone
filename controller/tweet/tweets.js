8a9a99999dafa6349313bffeeb2b95ac9cbd1896
mahdi <mahdimamashli1383@gmail.com>
2024-03-24 04:18:30 +0330

    "Update tweet and user documents when liking/unliking a tweet; handle retweet logic"
    This commit includes changes to the `likeTweet` function in the `tweets.js` controller for the tweet feature. It updates the tweet and user documents when a user likes or unlikes a tweet, and it handles retweet logic and updates likes accordingly. The commit removes some console logs.

--- controller/tweet/tweets.js
+++ controller/tweet/tweets.js
@@ -108,13 +108,7 @@ exports.likeTweet = async (req, { getJsonHandler }) => {
 
     // Handle retweet logic (if applicable) and update likes accordingly
     const option = tweetLikedByUser ? "$pull" : "$addToSet";
-    // console.log('option=>',option);
      await handleRetweet(tweet, user.userId, option)
-    // const infoUser  = user.likedTweets.includes(updatedOriginalTweet._id)
-    // console.log(' user.likedTweets =>', user.likedTweets );
-    // console.log(' infoUser =>', infoUser );
-    // console.log(' updatedOriginalTweet._id =>', updatedOriginalTweet._id );
-    // console.log(' updatedOriginalTweet =>', updatedOriginalTweet );
     // Create the update queries for the user and the tweet
     const { UserQuery, TweetQuery } = generateTweetQueries(
       option,
