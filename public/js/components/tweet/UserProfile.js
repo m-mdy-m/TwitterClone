33ed84ef9b455dce7a8ffd0335f516e0865e554b
mahdi <mahdimamashli1383@gmail.com>
2024-03-25 00:37:27 +0330

    Update UserProfile component to include retweeted username and optional border class
    The UserProfile component has been updated to include a new prop, `retweetedUsername`, which is used to display the username of the person who retweeted the tweet. Additionally, an optional `classNameBorder` prop has been added to conditionally apply a border class to the retweeted icon.

--- public/js/components/tweet/UserProfile.js
+++ public/js/components/tweet/UserProfile.js
@@ -1,13 +1,13 @@
 import { Profile } from "../common/Profile.js";
 
-export function UserProfile({ username, profile, className }) {
+export function UserProfile({ username, profile, className,retweetedUsername,classNameBorder='hidden' }) {
   return `
   <div class="container__profile-users flex ">
-      <div class="flex justify-center items-center absolute top-2 left-[4px]  user-retweeted">
-          <a href="#" class="hover:underline text-[#fff] text-[8px] flex items-center">@m__mdy__m</a>
+      <div class="justify-center items-center absolute top-2 left-[4px] ${classNameBorder} user-retweeted">
+          <a href="#" class="hover:underline text-[#fff] text-[8px] flex items-center">@${retweetedUsername}</a>
       </div>
       <div class="bg-gradient-to-tr from-[#222224] to-[#262629] rounded-full profile-user flex justify-center items-center border border-[#403e42] shadow-lg cursor-pointer duration-100 transition-all hover:scale-105">
-        <img src="/assets/icon/nav/retweeted.svg" alt="" class="w-8 h-8 absolute hover:-z-10 icon-retweet">
+        <img src="/assets/icon/nav/retweeted.svg" alt="" class="w-8 h-8 absolute hover:-z-10 icon-retweet ${classNameBorder}">
         ${Profile({ profile, username, className })}
       </div>
   </div>
