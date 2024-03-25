646d23b0d93692f98ae3c354a685d9d80edc0453
mahdi <mahdimamashli1383@gmail.com>
2024-03-25 13:55:04 +0330

    "Add new endpoint for finding user by ID and modify existing endpoint to accept optional query parameter for user ID"
    This commit adds a new function `findUserById` in the `info.js` controller file, which handles finding a user by their ID passed in the request query parameters. The existing `findUser` function has been modified to accept an optional query parameter for the user ID. Additionally, the `routes/api/info.js` file has been updated to include a new route for finding a user by their ID and to modify the existing route to accept an optional query parameter for the user ID.

--- controller/info/info.js
+++ controller/info/info.js
@@ -3,20 +3,6 @@ const User = require('../../model/User')
 exports.findUser = async (req, { status, getJsonHandler }) => {
   const { notFound, internalServerError } = getJsonHandler();
   try {
-    const id = req.query('id')
-    console.log('id=>',id);
-    if(id){
-      const user = await User.findById(id)
-      if (!user) {
-        // If user is not found, send a 404 Not Found response
-        return notFound("User not found");
-      }
-      // If user is found, send the user object as a JSON response
-      return status(200).json({
-        success: true,
-        data: user,
-      });
-    }
     const user = req.user;
     if (!user) {
       // If user is not found, send a 404 Not Found response
@@ -32,6 +18,32 @@ exports.findUser = async (req, { status, getJsonHandler }) => {
     internalServerError("Something went wrong. Please try again later.");
   }
 };
+
+
+exports.findUserById = async (req,{status, getJsonHandler})=>{
+  const { notFound, internalServerError } = getJsonHandler();
+  try {
+    const id = req.getQueryParam('id');
+    if (!id) {
+      // If user is not found, send a 404 Not Found response
+      return notFound("User not found");
+    }
+    const user = await User.findById(id)
+    if (!user) {
+      // If user is not found, send a 404 Not Found response
+      return notFound("User not found");
+    }
+    // If user is found, send the user object as a JSON response
+    return status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    // Send a 500 Internal Server Error response with a generic error message
+    internalServerError("Something went wrong. Please try again later.");
+  }
+}
+
 
 exports.findTweet = async (req, { getJsonHandler }) => {
   const { notFound, success, internalServerError } = getJsonHandler();
