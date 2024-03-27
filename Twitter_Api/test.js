7bbea2d4f997514297a8c4ddc7ad16ae35884985
mahdi <mahdimamashli1383@gmail.com>
2024-03-27 12:38:17 +0330

    Update Twitter API credentials in test.js
    - Change variable names for `clientId`, `clientSecret`, `appKey`, and `appSecret` to match the required naming convention in the Twitter API library.
    - No functional changes, just variable name updates for consistency.

--- Twitter_Api/test.js
+++ Twitter_Api/test.js
@@ -2,19 +2,16 @@ const { TwitterApi } = require("twitter-api-v2");
 require("dotenv").config();
 
 const readOnlyClient = new TwitterApi({
-  clientId: process.env.Client_ID,
-  clientSecret:process.env.Client_Secret,
-  appKey: process.env.API_kEY,
-  appSecret: process.env.API_kEY_SECRET,
+  clientId: process.env.CLIENT_ID,
+  clientSecret:process.env.CLIENT_SECRET,
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_KEY_SECRET,
   accessToken: process.env.ACCESS_TOKEN,
   accessSecret: process.env.ACCESS_TOKEN_SECRET,
 });
 
 (async () => {
   try {
-    // const user = await readOnlyClient.v2.userByUsername("m__mdy__m");
-    // console.log("user =>", user);
-    // Since this is a read-only client, you won't be able to tweet
     const tweet = await readOnlyClient.v2.tweet("hi this is test from nodejs");
     console.log("tweet =>", tweet);
   } catch (error) {
