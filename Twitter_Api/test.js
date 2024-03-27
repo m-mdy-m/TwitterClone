8f792baa6a0c5199625a4e2d32037b61d275dcb6
mahdi <mahdimamashli1383@gmail.com>
2024-03-27 12:36:36 +0330

    Change from TwitterApi client instantiation with bearer token to using client credentials and access tokens Comment out user lookup and tweet creation using read-only client as it does not allow tweet creation Fix console.error to consistently display error object

--- Twitter_Api/test.js
+++ Twitter_Api/test.js
@@ -1,19 +1,23 @@
 const { TwitterApi } = require("twitter-api-v2");
 require("dotenv").config();
-const client = new TwitterApi(process.env.BEARER_TOKEN);
 
-const readOnlyClient = client.readOnly;
+const readOnlyClient = new TwitterApi({
+  clientId: process.env.Client_ID,
+  clientSecret:process.env.Client_Secret,
+  appKey: process.env.API_kEY,
+  appSecret: process.env.API_kEY_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
 
 (async () => {
   try {
-    const user = await readOnlyClient.v2.userByUsername("m__mdy_m");
-    console.log("User:", user);
-
-    const tweetResponse = await readOnlyClient.v2.tweet(
-      "hi this is test in nodejs"
-    );
-    console.log("Tweet response:", tweetResponse);
+    // const user = await readOnlyClient.v2.userByUsername("m__mdy__m");
+    // console.log("user =>", user);
+    // Since this is a read-only client, you won't be able to tweet
+    const tweet = await readOnlyClient.v2.tweet("hi this is test from nodejs");
+    console.log("tweet =>", tweet);
   } catch (error) {
-    console.error("Error:", error);
+    console.error(error);
   }
 })();
