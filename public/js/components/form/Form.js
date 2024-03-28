3a139895fe7fa697bde746238afcdb19cbba5220
mahdi <mahdimamashli1383@gmail.com>
2024-03-28 13:59:44 +0330

    "Update main.css with new classes and modify form spacing; update InputField spacing in InputField.js"
    This commit message describes the changes in the code diff without making any interpretations. It mentions the addition of new classes and modification of spacing in main.css, as well as the modification of spacing in InputField.js. The message is concise and accurately reflects the changes made in the code.

--- public/js/components/form/Form.js
+++ public/js/components/form/Form.js
@@ -1,7 +1,7 @@
 export function Form({ title, action, method, children }) {
   return `
     <div class="flex bg-[#27374D] w-full blur-bg-login h-full justify-center items-center ">
-      <form id="registerForm" action="${action}" method="${method}" class="p-4 w-3/5 h-5/6 flex flex-col items-center justify-center  [&>*]:my-1 ">
+      <form id="registerForm" action="${action}" method="${method}" class="p-4 w-3/5 h-5/6 flex flex-col items-center justify-center  [&>*]:my-3 ">
         <h1 class="text-3xl text-center mb-6 text-gray-300">${title}</h1>
         ${children}
       </form>
