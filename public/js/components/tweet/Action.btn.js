b85ff703d78650c1cc52b8cd72b1e6429c759a7b
mahdi <mahdimamashli1383@gmail.com>
2024-03-28 22:40:25 +0330

    Add border to the top of the ActionButtons div and update main.css with a new class for border-t; update PostContent div in PostContent.js to remove border-b.
    The diff adds a border to the top of the ActionButtons div and creates a new class for border-t in main.css. It also removes the border-b from the PostContent div in PostContent.js.

--- public/js/components/tweet/Action.btn.js
+++ public/js/components/tweet/Action.btn.js
@@ -2,7 +2,7 @@ import { Icon } from "./Icon.js";
 const styleIcon ='w-5 h-5'
 export function ActionButtons({retweetCount,likeCount,srcLikeIcon,srcRetweetIcon}) {
   return `
-      <div class="flex w-full justify-start items-center pt-3 px-2 text-white [&>*]:cursor-pointer">
+      <div class="flex w-full justify-start items-center pt-3 px-2 text-white [&>*]:cursor-pointer border-t border-[#252424] ">
          ${Icon({src:'nav/chat-comment.svg',alt: 'Comment',className:'flex items-center',styleIcon , id: 'commentIcon',})}
          ${Icon({src:srcLikeIcon,alt: 'Like',className:'flex items-center',styleIcon , id: 'likeIcon',display:'block',count:likeCount })}
          ${Icon({src:srcRetweetIcon,alt: 'ReTweet',className:'flex items-center',styleIcon , id: 'retweetIcon',display:'block',count:retweetCount })}
