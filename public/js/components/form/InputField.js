4dfb6b8e55773b865bbfb16f9aa9961ab008951e
mahdi <mahdimamashli1383@gmail.com>
2024-03-28 14:25:05 +0330

    change auto fill Google input color

--- public/js/components/form/InputField.js
+++ public/js/components/form/InputField.js
@@ -1,7 +1,7 @@
 export function InputField({ type, name, placeholder, id }) {
   return `
-    <div class="form-group flex justify-center flex-col items-center w-full [&>input]:p-3">
-      <input type="${type}" name="${name}" placeholder="${placeholder}" class="input-form w-3/4 h-full bg-gray-700 text-white" id="${id}" value=""/>
+    <div class="form-group flex justify-center flex-col items-center w-full [&>input]:p-2">
+      <input type="${type}" name="${name}" placeholder="${placeholder}" class="input-form w-3/4  h-full bg-gray-700 text-white" id="${id}" value=""/>
       <i class="msg-error"></i>
     </div>
   `;
