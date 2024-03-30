d7a529b9e6ba24108e93a7e85a77be64fe8b9c49
mahdi <mahdimamashli1383@gmail.com>
2024-03-30 14:07:32 +0330

    add icon edit

--- public/js/components/Tweet.js
+++ public/js/components/Tweet.js
@@ -33,10 +33,14 @@ function Tweet({
           retweetedUsername,
         })}
         <div class="flex justify-center items-center  list__menu-icon w-8 h-8 transition-transform cursor-pointer relative transform hover:scale-110">
-           <div class="list__menu-tweet cursor-default [&>*]:opacity-0 [&>*]:cursor-pointer flex justify-center bg-gray-800 px-4 absolute right-0 -z-10 p-1 rounded-r-md rounded-l-xl transition-all">
+           <div class="list__menu-tweet cursor-default [&>*]:opacity-0 [&>*]:cursor-pointer [&>*]:mx-0.5 flex justify-center bg-gray-800 px-4 absolute right-0 -z-10 p-1 rounded-r-md rounded-l-xl transition-all">
                <!-- Bookmark Icon -->
                <svg class="bookmarkIcon w-6 h-6 ${bookmarkIcon}  transition-colors duration-300 ease-in-out" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                    <path fill-rule="evenodd" clip-rule="evenodd" d="M4 7.75V18C4 19.6481 5.88153 20.5889 7.2 19.6L10.8 16.9C11.5111 16.3667 12.4889 16.3667 13.2 16.9L16.8 19.6C18.1185 20.5889 20 19.6481 20 18V7.75H4ZM4 6.25H20V4C20 2.89543 19.1046 2 18 2H6C4.89543 2 4 2.89543 4 4V6.25Z" fill="currentColor"/>
+               </svg>
+               <!-- Edit Icon -->
+               <svg class="editIcon w-6 h-6 fill-current text-gray-400 hover:text-blue-500 transition duration-300 ease-in-out transform cursor-pointer" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                   <path fill-rule="evenodd" clip-rule="evenodd" d="M19.0711 4.59512C17.9827 3.50676 16.2181 3.50676 15.1298 4.59512L14.0201 5.70482L17.9751 9.65979L19.0848 8.55008C20.1732 7.46172 20.1732 5.69717 19.0711 4.59512ZM16.6062 11.0022L13.6521 7.04815L4.66795 16.0344C4.10741 16.5949 3.72903 17.311 3.58079 18.0898L3.15657 20.3182C3.02461 21.0114 3.63276 21.6196 4.32595 21.4877L6.55442 21.0635C7.33316 20.9152 8.04933 20.5362 8.60987 19.9757L16.6062 11.0022Z" fill="currentColor"/>
                </svg>
                <!-- Delete tweet icon -->
                <svg class="deleteIcon ${showDeleteIcon} w-6 h-6 text-gray-400 hover:text-red-500 transition-colors duration-300 ease-in-out" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
