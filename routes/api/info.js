f23b66c6a8e3a577fd46d6ddc0dc7f7af1a430ad
mahdi <mahdimamashli1383@gmail.com>
2024-04-01 20:35:56 +0330

    update

--- routes/api/info.js
+++ routes/api/info.js
@@ -1,6 +1,6 @@
 const route = require("xprz").Route();
 const { verifyToken } = $read("middleware/is-auth");
-const { findUser,findTweet,findUserById } = $read("controller/info/info");
+const { findUser,findTweet } = $read("controller/info/info");
 // Defining a GET route for fetching tweets
 route.route('/user-info').mid([verifyToken]).get(findUser);
 route.route("/tweet-info/:id").mid([verifyToken]).get(findTweet);
