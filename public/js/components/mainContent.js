7f2487fda81d1e9f73fc674a931675dab1f01b8e
mahdi <mahdimamashli1383@gmail.com>
2024-04-03 16:24:09 +0330

    Update mainContent.js to use switch statement instead of if-else if-else; add conditional check for currentPage starting with '/profile/'
    Before, there were multiple if-else if-else statements to handle different routes. Now, a switch statement is used instead for better readability. Also, a conditional check is added to handle currentPage starting with '/profile/'.

--- public/js/components/mainContent.js
+++ public/js/components/mainContent.js
@@ -1,17 +1,24 @@
 import { ProfilePage } from "./profile/ProfilePage.js";
 import { LoginForm, SignupForm } from "./form/FormHandler.js";
-import { HOME } from "./main/Home.js"
-let currentPage = window.location.pathname
+import { HOME } from "./main/Home.js";
+
+let currentPage = window.location.pathname;
+
+if (currentPage.startsWith('/profile/')) {
+    currentPage = '/profile/';
+}
+
 export function mainContent() {
-    if (currentPage === '/home') {
-        return HOME({ username: "m__mdy__m", profilePic: '/assets/images/profilePic.png', profileStory: "/assets/icon/nav/user.svg" });
-    } else if (currentPage === '/auth/login') {
-        return LoginForm();
-    } else if (currentPage === '/auth/signup') {
-        return SignupForm();
-    } else if (currentPage.startsWith('/profile/')) {
-        return ProfilePage();
-    } else {
-        return "";
+    switch (currentPage) {
+        case '/home':
+            return HOME({ username:"m__mdy__m", profilePic: '/assets/images/profilePic.png', profileStory: "/assets/icon/nav/user.svg" });
+        case '/auth/login':
+            return LoginForm();
+        case '/auth/signup':
+            return SignupForm();
+        case '/profile/':
+            return ProfilePage();
+        default:
+            return "";
     }
 }
