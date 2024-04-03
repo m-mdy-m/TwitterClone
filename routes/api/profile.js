07201577e0cf2305c603b21386235696ba070444
mahdi <mahdimamashli1383@gmail.com>
2024-04-03 14:57:23 +0330

    Add verifyToken middleware to /api/profile/:username route
    Add verifyToken middleware to protect the /api/profile/:username route. This ensures that only authenticated requests can access the endpoint.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,9 @@
+const { verifyToken } = require('../../middleware/is-auth')
+
 const {group, expose} = require('xprz').Route()
 const { getProfile } = $read('controller/profile/profile')
 group('/api',({route})=>{
-    route('/profile/:username').get(getProfile)
+    route('/profile/:username').mid(verifyToken).get(getProfile)
 })
 
 
