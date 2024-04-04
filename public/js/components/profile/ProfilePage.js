4e8cee7ee107c0bd13e31a178ddabb2feb28a51a
mahdi <mahdimamashli1383@gmail.com>
2024-04-04 12:32:01 +0330

    CSS classes added for positioning elements; Tailwind CSS classes updated in ProfilePage and Icon components; sizeText and classNameContent props added to Icon component.
    The CSS file has new classes for positioning elements using negative margins, and the ProfilePage component has updated Tailwind CSS classes for better spacing and layout. The Icon component now includes sizeText and classNameContent props to customize the text size and position of the icon's content.

--- public/js/components/profile/ProfilePage.js
+++ public/js/components/profile/ProfilePage.js
@@ -4,11 +4,11 @@ export function ProfilePage() {
   return `
     <div class="w-full h-full grid grid-cols-profile ">
         <div class="bg-green-700"></div>
-        <div class="bg-blue-400 flex first-letter:justify-start flex-col items-center [&>*]:my-2 [&>*]:flex-col ">
-            ${Icon({src:'profile_nav/Analyze.svg',alt:"Analyze", content:'Analyze', display:"block", className:"flex justify-center items-center flex-col"})}
-            ${Icon({src:'profile_nav/EditProfile.svg',alt:"EditProfile", content:'EditProfile', display:"block", className:"flex justify-center items-center flex-col"})}
-            ${Icon({src:'profile_nav/Friends.svg',alt:"Friends", content:'Friends', display:"block", className:"flex justify-center items-center flex-col"})}
-            ${Icon({src:'profile_nav/DeleteAccount.svg',alt:"DeleteAccount", content:'DeleteAccount', display:"block", className:"flex justify-center items-center flex-col"})}
+        <div class="nav__profile-user bg-blue-400 flex first-letter:justify-start flex-col items-center [&>*]:my-4 [&>*]:flex-col ">
+            ${Icon({src:'profile_nav/Analyze.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"Analyze", content:'Analyze', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
+            ${Icon({src:'profile_nav/EditProfile.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"EditProfile", content:'EditProfile', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
+            ${Icon({src:'profile_nav/Friends.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"Friends", content:'Friends', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
+            ${Icon({src:'profile_nav/DeleteAccount.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"DeleteAccount", content:'DeleteAccount', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
         </div>
     </div>
   `;
--- public/js/components/tweet/Icon.js
+++ public/js/components/tweet/Icon.js
@@ -1,8 +1,8 @@
-export function Icon({ src, alt, className =  "flex justify-center items-center", id = "",styleIcon="",display='hidden',content=''}) {
+export function Icon({ src, alt, className =  "mr-4 flex justify-center items-center", id = "",styleIcon="",display='hidden',content='',sizeText='text-[10px]',classNameContent="absolute -bottom-1 -right-1"}) {
   return `
-    <div class="${className} relative  icons mr-4 icon-container transform transition duration-150 hover:scale-105">
+    <div class="${className} relative icons icon-container transform transition duration-150 hover:scale-105">
       <img src="/assets/icon/${src}" alt="${alt}" id="${id}" class="${styleIcon}">
-      <p class="${display} absolute -bottom-1 -right-1 text-[#5c6065] rounded-full flex items-center justify-center text-[10px]">${content}</p>
+      <p class="${display} ${classNameContent} text-[#5c6065] rounded-full flex items-center justify-center ${sizeText}">${content}</p>
     </div>
     `;
 }
