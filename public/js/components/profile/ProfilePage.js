dd88c004070309ce420ba5371b2c503cabe67b46
mahdi <mahdimamashli1383@gmail.com>
2024-04-04 12:34:28 +0330

    "Replace Icon components in nav\_profile-user with navUserIcon"
    This commit removes the usage of multiple Icon components in the ProfilePage component's nav\_profile-user div and replaces them with a single call to navUserIcon(). This new function presumably handles the rendering of the icons in a more concise or reusable way.

--- public/js/components/profile/ProfilePage.js
+++ public/js/components/profile/ProfilePage.js
@@ -1,15 +1,10 @@
-import { Icon } from "../tweet/Icon.js";
+import { navUserIcon } from "./nav/nav_profile.js";
 
 export function ProfilePage() {
   return `
     <div class="w-full h-full grid grid-cols-profile ">
         <div class="bg-green-700"></div>
-        <div class="nav__profile-user bg-blue-400 flex first-letter:justify-start flex-col items-center [&>*]:my-4 [&>*]:flex-col ">
-            ${Icon({src:'profile_nav/Analyze.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"Analyze", content:'Analyze', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
-            ${Icon({src:'profile_nav/EditProfile.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"EditProfile", content:'EditProfile', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
-            ${Icon({src:'profile_nav/Friends.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"Friends", content:'Friends', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
-            ${Icon({src:'profile_nav/DeleteAccount.svg',sizeText:'text-xs mavis',classNameContent:'absolute -bottom-3',styleIcon:"w-8 h-8",alt:"DeleteAccount", content:'DeleteAccount', display:"block", className:"flex justify-center items-center flex-col cursor-pointer "})}
-        </div>
+        ${navUserIcon()}
     </div>
   `;
 }
