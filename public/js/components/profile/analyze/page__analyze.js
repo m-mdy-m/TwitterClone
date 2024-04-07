5f683c53f02bdedf8147f5dd831fd00fac4bf49d
mahdi <mahdimamashli1383@gmail.com>
2024-04-07 19:29:10 +0330

    "Add opacity transition and z-index to time period buttons in Analyze page; update CSS selectors"
    This commit includes changes to both the CSS and JS files related to the Analyze page in a web application. In the CSS file, a new selector is added for the time period buttons, with a class that sets the initial opacity to 0 and assigns a higher z-index to the first button. In the JS file, the time period buttons now have the new class, and the first button also has the 'first:relative' and 'first:opacity-100' classes. The overall goal of these changes is to add a smooth opacity transition and ensure the correct button is always displayed on top.

--- public/js/components/profile/analyze/page__analyze.js
+++ public/js/components/profile/analyze/page__analyze.js
@@ -1,12 +1,12 @@
 export function page_analyze() {
     return `
-      <div class="absolute top-3 right-5 p-2 px-4 rounded-md  bg-red-700">
-        <div class="flex justify-center items-center flex-col first:!z-30  [&>*]:text-white first:!relative [&>*]:opacity-0 [&>*]:absolute">
-          <button>Month</button>
-          <button>Week</button>
-          <button>Year</button>
+    <div class="absolute top-3 right-5 p-2 px-3 rounded-md bg-red-700">
+        <div class="flex justify-center items-center flex-col">
+          <button class="first:opacity-100 first:relative opacity-0 absolute">Month</button>
+          <button class="first:relative first:opacity-100 opacity-0 absolute">Week</button>
+          <button class="first:relative first:opacity-100 opacity-0 absolute">Year</button>
         </div>
-      </div>
+     </div>
     <div class="flex justify-center items-center">
       <canvas id="chart__all-post"></canvas>
     </div>
