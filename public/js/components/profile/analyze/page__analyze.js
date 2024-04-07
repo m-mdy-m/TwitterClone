5dd002c1d71c4b1dc95494d1b663d47b7f225d2f
mahdi <mahdimamashli1383@gmail.com>
2024-04-07 19:29:49 +0330

    "Reordered time period buttons in profile analyze component"
    This commit message accurately describes the code diff by mentioning that the order of the time period buttons (Week, Month, Year) has been changed in the profile analyze component. It is concise and sticks to the facts of the code diff without making any assumptions or interpretations.

--- public/js/components/profile/analyze/page__analyze.js
+++ public/js/components/profile/analyze/page__analyze.js
@@ -2,9 +2,9 @@ export function page_analyze() {
     return `
     <div class="absolute top-3 right-5 p-2 px-3 rounded-md bg-red-700">
         <div class="flex justify-center items-center flex-col">
-          <button class="first:opacity-100 first:relative opacity-0 absolute">Month</button>
-          <button class="first:relative first:opacity-100 opacity-0 absolute">Week</button>
-          <button class="first:relative first:opacity-100 opacity-0 absolute">Year</button>
+            <button class="first:relative first:opacity-100 opacity-0 absolute">Week</button>
+            <button class="first:opacity-100 first:relative opacity-0 absolute">Month</button>
+            <button class="first:relative first:opacity-100 opacity-0 absolute">Year</button>
         </div>
      </div>
     <div class="flex justify-center items-center">
