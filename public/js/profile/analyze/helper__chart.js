67473606e143be6398984a30a3265a3196409ffd
mahdi <mahdimamashli1383@gmail.com>
2024-04-08 19:16:49 +0330

    "Update chart dataset and options in ChartDataManager"
    This commit updates the chart dataset and options in the ChartDataManager class. The 'views' dataset has been modified to include additional properties such as backgroundColor, pointBackgroundColor, etc. The 'scales' and 'plugins' sections have been added to the 'options' object. The 'animation' property has also been added to the 'options' object.

--- public/js/profile/analyze/helper__chart.js
+++ public/js/profile/analyze/helper__chart.js
@@ -39,18 +39,48 @@ export class ChartDataManager {
       type: "line",
       data: {
         labels: this.weakLabels,
-        datasets: [{
-          label: "views",
-          data: this.views, // views all post
-          borderColor: "green",
-          borderWidth: 1,
-          tension: 0.5,
-        }],
+        datasets: [
+          {
+            label: "Views",
+            data: this.views,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 2,
+            pointBackgroundColor: "rgba(75, 192, 192, 1)",
+            pointBorderColor: "rgba(75, 192, 192, 1)",
+            pointBorderWidth: 2,
+            pointRadius: 2,
+            pointHoverRadius: 7,
+            lineTension: 0.4,
+          },
+        ],
       },
       options: {
+        animation: {
+          duration: 2000,
+          easing: "easeInOutQuad",
+        },
         scales: {
           y: {
-            beginAtZero: true,
+            display: false,
+          },
+          x: {
+            grid: {
+              color: "rgba(255, 255, 255, 0)",
+            },
+            ticks: {
+              color: "rgba(255, 255, 255, 0.7)",
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: "rgba(255, 255, 255, 0.7)",
+              font: {
+                size: 14,
+              },
+            },
           },
         },
       },
