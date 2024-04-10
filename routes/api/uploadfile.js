e9c65ba75ce1d8df1a8ad3b39c3e756308ea81bd
mahdi <mahdimamashli1383@gmail.com>
2024-04-11 02:27:43 +0330

    fix bug upload image

--- routes/api/uploadfile.js
+++ routes/api/uploadfile.js
@@ -1,7 +1,7 @@
 const { expose, route } = require("xprz").Route();
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs").promises
+const fs = require("fs").promises;
 const User = require("../../model/User");
 const TweetUserManager = require("../../utils/helper");
 const storage = multer.diskStorage({
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     const uploadPath = path.join(process.cwd(), "public", "upload");
     try {
       await fs.mkdir(uploadPath, { recursive: true });
-      cb(null, '/public/upload');
+      cb(null, "/public/upload");
     } catch (err) {
       console.error("Error creating upload directory:", err);
       return cb(err);
@@ -52,18 +52,18 @@ route("/upload/profile/:userId")
       return badRequest("No file uploaded.");
     }
     const userId = ctx.param("userId");
-    const modifiedPath = path
-      .normalize(ctx.file.path)
+    console.log('ctx.file:',ctx.file)
+    const modifiedPath = ctx.file.path
       .replace(/^\\public\\/, "/")
       .replace(/\\/g, "/");
-    const user = await User.findByIdAndUpdate(userId, {
-      profilePic: modifiedPath,
-    });
+    const user =await User.findById(userId)
     if (!user) {
       return notFound("User not found");
     }
+    user.profilePic = modifiedPath
+    await user.save()
     const tweetManager = new TweetUserManager(ctx, ctx.jsonSender);
-    const tokens = await tweetManager.saveUser(user)
-    success("Profile picture uploaded successfully",{tokens: tokens});
+    const tokens = await tweetManager.saveUser(user);
+    success("Profile picture uploaded successfully", { tokens: tokens });
   });
 module.exports = expose;
