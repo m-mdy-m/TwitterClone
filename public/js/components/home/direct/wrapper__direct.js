feb71d39299f6947b2421bfbce7a79943b6432e6
mahdi <mahdimamashli1383@gmail.com>
2024-04-12 00:03:25 +0330

    complete chat box

--- public/js/components/home/direct/wrapper__direct.js
+++ public/js/components/home/direct/wrapper__direct.js
@@ -1,12 +1,12 @@
 export function  template_direct({bio,username,profile}){
     return `
-    <div class="w-full my-2 p-2 px-4  delay-75 bg-[#11101076] hover:ml-2 hover:scale-105  hover:bg-[#262525a1] cursor-pointer transition-all rounded-r-lg flex justify-start items-center border-b border-[#252424]">
-    <div class="w-12 h-12">
-        <img src="${profile}" alt="" class="rounded-full w-full h-full object-cover">
-    </div>
-    <div class="flex justify-center flex-col text-[#ffffffee] pl-2">
-        <h1 class="text-sm font-Aktiv__medium">${username}</h1>
-        <p class="text-xs font-mavis__bold">${bio}</p>
-    </div>
+    <div data-username="${username}" class="w-full my-2 p-2 px-4  delay-75 bg-[#11101076] hover:ml-2 hover:scale-105  hover:bg-[#262525a1] cursor-pointer transition-all rounded-r-lg flex justify-start items-center border-b border-[#252424]">
+        <div class="w-12 h-12 border border-[#403e42] rounded-full p-0.5">
+            <img src="${profile}" alt="" class=" rounded-full w-full h-full object-cover">
+        </div>
+        <div class="flex justify-center flex-col text-[#ffffffee] pl-2">
+            <h1 class="text-sm font-Aktiv__medium">${username}</h1>
+            <p class="text-xs font-mavis__bold">${bio}</p>
+        </div>
     </div>`
 }
