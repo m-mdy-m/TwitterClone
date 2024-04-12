a5680291a992ebbe61b799bd112042691500138b
mahdi <mahdimamashli1383@gmail.com>
2024-04-12 12:47:43 +0330

    Update chat UI, add message scrolling, and increase message container width
    The changes include:
    
    * Updating the chat UI with a flex container and adjusting the positioning of message sender and recipient containers.
    * Adding scroll

--- public/js/components/home/direct/chat_template.js
+++ public/js/components/home/direct/chat_template.js
@@ -2,19 +2,23 @@ import { recipient, sender } from "./messageTemplates.js";
 
 export function chat_template({ img }) {
   return `
-    <div class="pr-4 h-full w-full">
-        <div class="grid grid-rows-box_tweet h-full w-full bg-[#1110109a] border-[#111010] rounded-xl">
-            <div class="w-full h-full p-2 origin-bottom ">
+    <div class="pr-4 w-full h-full">
+        <div class="flex flex-col h-full justify-between w-full bg-[#1110109a] border-[#252424] rounded-xl">
+            <div class="w-full max-h-24 h-24 p-2 origin-bottom ">
                 <img src="${img}" class="object-cover w-full h-full rounded-md origin-bottom ">
             </div>
-            <div class="grid grid-rows-chat_box">
-                <div class="m-0 p-0 overflow-x-scroll border border-gray-300 bg-red-600 rounded-xl relative font-Aktiv__regula">
+            <div class="grid grid-rows-chat_box h-full ">
+                <div class="overflow-x-scroll font-Aktiv__regula h-96 pl-2 rounded-lg scrollbar__hidden">
+                        ${sender()}
+                        ${recipient()}
+                        ${sender()}
+                        ${recipient()}
                         ${sender()}
                         ${recipient()}
                 </div>
                 <div class="flex justify-center items-center relative ">
-                    <button class="bg-sky-400 hover:bg-sky-900 absolute transition-all duration-150 px-2 py-1 left-1 h-3/4 rounded-md capitalize">send</button>
-                    <textarea placeholder="What's happening?" class="absolute bg-transparent right-0 px-2 ml-4 rounded-md w-11/12 border-r-transparent border-l-transparent border-b border-[#161617] text-gray-300 outline-none transition-all focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 resize-none overflow-y-hidden"></textarea>
+                    <button class="bg-sky-400 hover:bg-sky-900 transition-all ml-2 mb-2 duration-150 px-4 py-2 h-3/4 rounded-md capitalize">send</button>
+                    <textarea placeholder="What's happening?" class="bg-transparent mx-4 right-0 px-2 mb-2 rounded-md w-11/12 border-t-transparent border border-[#161617] text-gray-300 outline-none transition-all focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 resize-none overflow-y-hidden"></textarea>
                 </div>
             </div>
         </div>
--- public/js/components/home/direct/messageTemplates.js
+++ public/js/components/home/direct/messageTemplates.js
@@ -1,7 +1,7 @@
 export function sender(){
     return `
 <div class="flex justify-end">
-    <div class="message-sender bg-blue-500 text-white max-w-[70%]">
+    <div class="message-sender bg-blue-500 text-white max-w-96">
         <p>Hi Mahdi, are you okay?</p>
     </div>
 </div>`
@@ -10,7 +10,7 @@ export function sender(){
 export function recipient(){
     return `
     <div class="flex justify-start">
-        <div class="message-recipient bg-gray-200 text-black max-w-[70%]">
+        <div class="message-recipient bg-gray-200 text-black max-w-96">
             <p>Hi there! Yes, I'm doing fine. How about you?</p>
         </div>
     </div>`
