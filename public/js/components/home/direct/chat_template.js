6e763a87e28436efc090bd1a6721432ec2d1adb1
mahdi <mahdimamashli1383@gmail.com>
2024-04-12 11:55:31 +0330

    Update chat template with message sender and recipient styles; add utility classes to main.css
    * Adds separate message sender and recipient styles in chat\_template.js
    * Adds utility classes for margin, padding, border, background color,

--- public/js/components/home/direct/chat_template.js
+++ public/js/components/home/direct/chat_template.js
@@ -1,16 +1,16 @@
-export function chat_template({img}){
-    return `
+import { recipient, sender } from "./messageTemplates.js";
+
+export function chat_template({ img }) {
+  return `
     <div class="pr-4 h-full w-full">
         <div class="grid grid-rows-box_tweet h-full w-full bg-[#1110109a] border-[#111010] rounded-xl">
             <div class="w-full h-full p-2 origin-bottom ">
                 <img src="${img}" class="object-cover w-full h-full rounded-md origin-bottom ">
             </div>
             <div class="grid grid-rows-chat_box">
-                <div class="border-4 border-[#3a3c3d] bg-red-600 rounded-xl relative">
-                    <div class="bg-green-300 w-44 h-8 font-Aktiv__regular p-1 rounded-md block absolute bottom-2 right-2">
-                        <p> hi mahdi are you okay? </p>
-                    </div>
-
+                <div class="m-0 p-0 overflow-x-scroll border border-gray-300 bg-red-600 rounded-xl relative font-Aktiv__regula">
+                        ${sender()}
+                        ${recipient()}
                 </div>
                 <div class="flex justify-center items-center relative ">
                     <button class="bg-sky-400 hover:bg-sky-900 absolute transition-all duration-150 px-2 py-1 left-1 h-3/4 rounded-md capitalize">send</button>
@@ -19,5 +19,5 @@ export function chat_template({img}){
             </div>
         </div>
     </div>
-    `
+    `;
 }
---
+++ public/js/components/home/direct/messageTemplates.js
@@ -0,0 +1,17 @@
+export function sender(){
+    return `
+<div class="flex justify-end">
+    <div class="message-sender bg-blue-500 text-white max-w-[70%]">
+        <p>Hi Mahdi, are you okay?</p>
+    </div>
+</div>`
+}
+
+export function recipient(){
+    return `
+    <div class="flex justify-start">
+        <div class="message-recipient bg-gray-200 text-black max-w-[70%]">
+            <p>Hi there! Yes, I'm doing fine. How about you?</p>
+        </div>
+    </div>`
+}
